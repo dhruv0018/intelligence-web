c24c98357826098471165d3025b486b5fb89786c
Jason Morganson <jason@morganson.me>
2013-09-16 16:45:59 -0400

    Add angular mocks for testing

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
         basePath: '',
 
         files: [
-            'prod/*.js',
+            'vendor/angular-mocks/angular-mocks.js',
             'test/unit/**/*.js',
             {pattern: 'src/**/*.js', included: false},
             {pattern: 'lib/**/*.js', included: false}
