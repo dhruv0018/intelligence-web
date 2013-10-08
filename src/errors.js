471dac69a1fa06d499702f8d185a50e5a735c1d5
Jason Morganson <jason@morganson.me>
2013-10-08 08:33:43 -0400

    Adds hook for built-in browser error handling function

--- src/errors.js
+++ src/errors.js
@@ -1,6 +1,24 @@
 var IntelligenceWebClient = require('./app');
 
 /**
+ * Browser error handler function. This is the default browser error handler
+ * function. In HTML5 the 4th and 5th parameters are available, giving access
+ * to the actual error. This is dependent on browser implementation and therefore
+ * may not be available.
+ * @param {String} message - error message
+ * @param {String} url - location where the error originated from
+ * @param {Number} line - line number of the occurrence
+ * @param {Number} column - column number of the occurrence
+ * @param {Error} error - actual error object
+ */
+window.onerror = function(message, url, line, column, error) {
+
+    /* If no error object is present create one. */
+    error = error || new Error(message + ' at ' + line + ':' + column + ' in ' + url);
+
+};
+
+/**
  * Creates decorator hook to Angular's exception handler
  * Uses a decorator to intercept the built-in Angular exception handler. This
  * allows other actions to be performed when Angular captures an exception.
