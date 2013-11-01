255a060ca3338bf7f2356a23da29c134a67f1bef
Jason Morganson <jason@morganson.me>
2013-11-01 15:03:37 -0400

    Updates schools factory with simplified save pattern

--- src/services/schools.js
+++ src/services/schools.js
@@ -25,8 +25,13 @@ IntelligenceWebClient.factory('SchoolsFactory', [
 
                 school = school || self;
 
-                if (school.id) self.resource.update(school);
-                else self.resource.create(school);
+                if (school.id) school.$update();
+
+                else {
+
+                    var newSchool = new SchoolsResource(school);
+                    newSchool.$create();
+                }
             }
         };
 
