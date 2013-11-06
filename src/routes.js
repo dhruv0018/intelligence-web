2e24384ebac2f860aad54097b601e6c68cf87417
Jason Morganson <jason@krossover.com>
2013-11-06 17:22:39 -0500

    Store logged in status in the root scope

--- src/routes.js
+++ src/routes.js
@@ -82,6 +82,8 @@ IntelligenceWebClient.run([
 
         $rootScope.$on('$stateChangeStart', function(event, to, toParams, from, fromParams) {
 
+            $rootScope.isLoggedIn = auth.isLoggedIn;
+
             /* If not accessing a public state and not logged in, then
              * redirect the user to login. */
             if (!to.public && !auth.isLoggedIn) {
