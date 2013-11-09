8a64b9abffb89bd84e86b684fa541e2e11b3efed
Jason Morganson <jason@krossover.com>
2013-11-08 19:13:12 -0500

    Readds school type and mascot

--- src/models/schools.js
+++ src/models/schools.js
@@ -1,5 +1,41 @@
 var IntelligenceWebClient = require('../app');
 
+var SCHOOL_TYPE = {
+
+    COLLEGE: 1,
+    UNIVERSITY: 1,
+    HIGH: 2,
+    HIGH_SCHOOL: 2,
+    OTHER: 3
+};
+
+IntelligenceWebClient.constant('SCHOOL_TYPE', SCHOOL_TYPE);
+
+var SCHOOL_TYPES = [
+
+    {
+        type: {
+
+            id: SCHOOL_TYPE.UNIVERSITY,
+            name: 'University/College'
+        },
+
+        type: {
+
+            id: SCHOOL_TYPE.HIGH_SCHOOL,
+            name: 'High School'
+        },
+
+        type: {
+
+            id: SCHOOL_TYPE.OTHER,
+            name: 'Other'
+        }
+    }
+];
+
+IntelligenceWebClient.constant('SCHOOL_TYPES', SCHOOL_TYPES);
+
 IntelligenceWebClient.factory('SchoolsResource', [
     'config', '$resource',
     function(config, $resource) {
