945cf2796b3a41c32fa43ea57750e191367e3b29
Jason Morganson <jason@krossover.com>
2013-12-08 17:48:17 -0500

    Removes isLoggedIn class and switches to flex layout for header

--- src/routes.js
+++ src/routes.js
@@ -77,9 +77,7 @@ IntelligenceWebClient.run([
 
         $rootScope.$on('$stateChangeStart', function(event, to, toParams, from, fromParams) {
 
-            $rootScope.isLoggedIn = auth.isLoggedIn;
-
-            /* If not accessing an authorized state and not logged in, then
+            /* If not accessing a public state and not logged in, then
              * redirect the user to login. */
             if (!authz.isPublic(to) && !auth.isLoggedIn) {
 
