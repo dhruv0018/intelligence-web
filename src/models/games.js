64340f260ee9a46b4ba947774b75dc876dc92c39
Jason Morganson <jason@krossover.com>
2014-01-13 13:46:34 -0500

    Create GAME_TYPE constant

--- src/models/games.js
+++ src/models/games.js
@@ -1,5 +1,14 @@
 var IntelligenceWebClient = require('../app');
 
+var GAME_TYPES = [
+
+    { name: 'Conference Game', value: 'conference', checked: true },
+    { name: 'Non-Conference Game', value: 'nonconfernece', checked: false },
+    { name: 'Playoff Game', value: 'playoff', checked: false },
+];
+
+IntelligenceWebClient.constant('GAME_TYPES', GAME_TYPES);
+
 IntelligenceWebClient.factory('GamesResource', [
     'config', '$resource',
     function(config, $resource) {
