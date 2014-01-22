da4566743751aaff55d1e770bad773792a799447
Jason Morganson <jason@krossover.com>
2014-01-21 22:39:51 -0500

    Overrides the flow.js chunk parameters to be more generic

--- src/flow.js
+++ src/flow.js
@@ -4,13 +4,33 @@ var IntelligenceWebClient = require('./app');
 
 IntelligenceWebClient.config([
     'config', 'flowFactoryProvider',
-    function (config, flowFactoryProvider) {
+    function (config, flow) {
 
-        flowFactoryProvider.defaults = {
+        flow.defaults = {
 
             target: config.kvs.uri + 'upload/part/',
             maxChunkRetries: 100,
             chunkRetryInterval: ONE_SECOND * 2
+        };
+
+        flow.factory = function(options) {
+
+            Flow.FlowChunk.prototype.getParams = function () {
+
+                return {
+
+                    chunkNumber: this.offset + 1,
+                    chunkSize: this.flowObj.opts.chunkSize,
+                    currentChunkSize: this.endByte - this.startByte,
+                    totalSize: this.fileObjSize,
+                    identifier: this.fileObj.uniqueIdentifier,
+                    filename: this.fileObj.name,
+                    relativePath: this.fileObj.relativePath,
+                    totalChunks: this.fileObj.chunks.length
+                };
+            };
+
+            return new Flow(options);
         };
     }
 ]);
