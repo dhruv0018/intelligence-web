b1bdabfb2ee147740bc77df91aa72ac561ef94e2
Jason Morganson <jason@krossover.com>
2014-01-30 14:27:43 -0500

    Adds a task to prep vendor files

--- Gruntfile.js
+++ Gruntfile.js
@@ -563,6 +563,13 @@ module.exports = function(grunt) {
     grunt.registerTask('deploy', ['dev', 'shell:dev']);
     grunt.registerTask('default', ['install', 'dev', 'connect:dev', 'watch']);
 
+    grunt.registerTask('prep', [
+        'copy:bootstrap',
+        'copy:bootstrapmin',
+        'concat:angular',
+        'concat:angularmin'
+    ]);
+
     grunt.registerTask('dev', [
         'less',
         'concat:angular',
