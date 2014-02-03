3b6c21d0e2588e1c0f9699292372f7d1e3167bd1
Jason Morganson <jason@krossover.com>
2014-02-03 14:33:08 -0500

    Adds Videogular

--- Gruntfile.js
+++ Gruntfile.js
@@ -402,7 +402,7 @@ module.exports = function(grunt) {
 
                             /* Redirect hash urls to index.html */
                             modRewrite([
-                                '!\\.html|\\.js|\\.css|\\.png$ /intelligence/index.html [L]'
+                                '!\\.html|\\.js|\\.css|\\.png|\\.mp4$ /intelligence/index.html [L]'
                             ]),
 
                             /* Serve static files. */
