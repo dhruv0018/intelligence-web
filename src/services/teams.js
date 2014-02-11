b1d14658056e25b19f670cfe096251af395b362b
Jason Morganson <jason@krossover.com>
2014-02-11 09:21:07 -0500

    Makes filter paramater optional in getList methods

--- src/services/teams.js
+++ src/services/teams.js
@@ -42,6 +42,14 @@ IntelligenceWebClient.factory('TeamsFactory', [
 
                 var self = this;
 
+                if (angular.isFunction(filter)) {
+
+                    index = error;
+                    error = success;
+                    success = filter;
+                    filter = null;
+                }
+
                 filter = filter || {};
                 filter.start = filter.start || 0;
                 filter.count = filter.count || 1000;
