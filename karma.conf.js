c05bd51b175b43be5c75afad0eaf931932d81e5d
Jason Morganson <jason@krossover.com>
2014-03-07 09:59:34 -0500

    Adds notifications to tests --skip-ci

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
 
         browsers: ['PhantomJS'],
 
-        reporters: ['progress', 'coverage'],
+        reporters: ['progress', 'coverage', 'osx'],
 
         coverageReporter: {
             dir: 'test/coverage',
