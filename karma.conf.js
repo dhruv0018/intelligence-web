aa65a87813da483eb59e05c933f8db9c83c7ec51
Jason Morganson <jason@krossover.com>
2014-03-12 13:21:11 -0400

    Use the build bundle for karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
         basePath: '',
 
         files: [
-            'dev/intelligence/*.js',
+            'build/bundle.js',
             'node_modules/angular-mocks/angular-mocks.js',
             'test/unit/**/*.js',
             {pattern: 'src/**/*.js', included: false},
