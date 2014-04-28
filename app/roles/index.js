a9a9eab1a66cd7ebdbdbd2203e05d3fb6daf4594
Jason Morganson <jason@krossover.com>
2014-04-28 18:53:23 -0400

    Update state

--- app/roles/index.js
+++ app/roles/index.js
@@ -78,7 +78,7 @@ Roles.controller('RolesController', [
             session.storeCurrentUser($scope.currentUser);
 
             $modalInstance.close();
-            $state.go('account');
+            $state.go('contact-info');
         };
     }
 ]);
