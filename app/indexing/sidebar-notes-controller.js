becba529cc17b5a45b85b82d9d9ab10642bb6eb5
Wes Pickett <wes@krossover.com>
2014-05-19 18:38:47 -0400

    added ability to add note in indexing

--- app/indexing/sidebar-notes-controller.js
+++ app/indexing/sidebar-notes-controller.js
@@ -13,9 +13,32 @@ var Indexing = angular.module('Indexing');
  * @name Sidebar.Notes.Controller
  * @type {Controller}
  */
-Indexing.controller('Indexing.Sidebar.Notes.Controller', [
-    function controller() {
+Indexing.controller('Indexing.Sidebar.Notes.Controller', ['$scope', 'GAME_NOTE_TYPES',
+    function controller($scope, GAME_NOTE_TYPES) {
+		
+		$scope.noteValues = ['Camera did not follow play', 'Jersey not visible', 'Gap in film', 'Scoreboard shot', 'Other'];
+		
+		$scope.selectedNoteText = $scope.noteValues[0];
+		
+		$scope.editSaveNote = function() {
+			
+			var noteValueToSave = $scope.selectedNoteText;
 
+			if ($scope.selectedNoteText === $scope.noteValues[$scope.noteValues.length-1]) {
+				noteValueToSave = $scope.otherNoteValue;
+			}
+
+			$scope.indexing.game.notes[GAME_NOTE_TYPES.INDEXER_NOTE] =  [{
+				'content': noteValueToSave,
+				'gameId': $scope.indexing.game.id,
+				'noteTypeId': GAME_NOTE_TYPES.INDEXER_NOTE,
+				'id': $scope.indexing.game.id,
+				'gameTime': $scope.VideoPlayer.videoElement[0].currentTime,
+			}];
+
+			$scope.indexing.game.save();
+
+		};
     }
 ]);
 
