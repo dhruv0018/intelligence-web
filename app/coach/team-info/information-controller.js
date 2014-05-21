01f3c6570d7006a20d39aa6d095e0a1aa6a6f64a
Michael <michael@krossover.com>
2014-05-21 11:39:21 -0400

    file upload with local reference

--- app/coach/team-info/information-controller.js
+++ app/coach/team-info/information-controller.js
@@ -16,12 +16,20 @@ var TeamInfo = angular.module('Coach.Team.Info');
 TeamInfo.controller('Coach.Team.Info.Information.controller', [
     '$rootScope', '$scope', '$state', '$http', 'config', 'GamesFactory', 'PlayersFactory', 'Coach.Data',
     function controller($rootScope, $scope, $state, $http, config, games, players, data) {
+        var reader = new FileReader();
 
-        data.then(function(data){
+        data.then(function(data) {
             $scope.data = data;
         });
 
-        $scope.upload = function(files){
+        $scope.upload = function(files) {
+            reader.readAsDataURL(files[0]);
+
+            reader.onload = function() {
+                $scope.data.coachTeam.imageUrl = reader.result;
+                $scope.$apply();
+            };
+
             var url = config.api.uri + 'teams/' + $scope.data.coachTeam.id + '/image/file';
 
             var data = new FormData();
@@ -32,7 +40,7 @@ TeamInfo.controller('Coach.Team.Info.Information.controller', [
                 headers: { 'Content-Type': undefined },
                 transformRequest: angular.identity
             })
-            .success(function(team){
+            .success(function(team) {
                 data.coachTeam = $scope.data.coachTeam = team;
             })
             .error(function() {
