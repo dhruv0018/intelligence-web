9c65a802099a8eee0b5e24f5c0865b93eb3bc919
Shehryar <ssh2690@gmail.com>
2014-05-27 15:45:20 -0400

    Creating a default plan now works, backend still needs adjustment

--- app/admin/platform/new-plan/index.js
+++ app/admin/platform/new-plan/index.js
@@ -29,8 +29,8 @@ NewPlan.run([
  * @type {Controller}
  */
 NewPlan.controller('NewPlanController', [
-    '$scope', '$state', '$modalInstance', 'SessionService', 'SportsFactory', 'LeaguesFactory',
-    function controller($scope, $state, $modalInstance, session, sports, leagues) {
+    '$scope', '$state', '$modalInstance', 'SessionService', 'SportsFactory', 'LeaguesFactory', 'PlansFactory',
+    function controller($scope, $state, $modalInstance, session, sports, leagues, plans) {
 
         $scope.sports = sports.getList({}, function(sports) {
             return sports;
@@ -42,11 +42,8 @@ NewPlan.controller('NewPlanController', [
         console.log($scope.leagues);
         console.log($scope.sports);
 
-        $scope.savePlan = function() {
-            console.log('save');
-
-            //
-            // grab the data, package it up in to the plans object and then create a plan
+        $scope.savePlan = function(plan) {
+            plans.save(plan);
         };
 
     }
