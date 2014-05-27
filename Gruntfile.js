7ab959b8a8083f9939fc6373eb069f003d5c9eab
Wes Pickett <wes@krossover.com>
2014-05-27 13:45:17 -0400

    moved moustap pause into gruntfile include

--- Gruntfile.js
+++ Gruntfile.js
@@ -172,8 +172,8 @@ module.exports = function(grunt) {
             mousetrap: {
                 src: [
                     'node_modules/Mousetrap/mousetrap.js',
-                    'node_modules/Mousetrap/plugins/global-bind/mousetrap-global-bind.js',
-                    'node_modules/Mousetrap/plugins/pause/mousetrap-pause.js'
+                    'node_modules/Mousetrap/plugins/pause/mousetrap-pause.js',
+                    'node_modules/Mousetrap/plugins/global-bind/mousetrap-global-bind.js'
                 ],
                 dest: 'build/mousetrap.js'
             },
