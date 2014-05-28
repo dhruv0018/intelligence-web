a152613c4c2357781ab91f4efce8a846ab7a71b3
Jason Morganson <jason@krossover.com>
2014-05-28 14:25:07 -0400

    Use ng-app for Batarang to work

--- src/main.js
+++ src/main.js
@@ -77,5 +77,5 @@ require('./directives/autotab');
 require('./directives/verify-password.js');
 require('./directives/krossover/videoplayer');
 
-angular.bootstrap(document, [package.name]);
+//angular.bootstrap(document, [package.name]);
 
