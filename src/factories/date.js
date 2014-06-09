256f0e8969da13858d639963aff5749254f8298a
Wes Pickett <wes.pickett@gmail.com>
2014-06-09 14:11:52 -0400

    added date with zero UTC time

---
+++ src/factories/date.js
@@ -0,0 +1,13 @@
+var package = require('../../package.json');
+
+/* Fetch angular from the browser scope */
+var angular = window.angular;
+
+var IntelligenceWebClient = angular.module(package.name);
+
+
+IntelligenceWebClient.factory('NewDate', function() {
+    var dateZeroTime = new Date();
+    dateZeroTime.setUTCHours(0,0,0,0);
+    return dateZeroTime;
+});
