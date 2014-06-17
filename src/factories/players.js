ec8c87585e67ba982398c8f6fcd255495953fbde
Michael <michael@krossover.com>
2014-06-17 14:30:50 -0400

    ability to pass in a custom sorting function

--- src/factories/players.js
+++ src/factories/players.js
@@ -143,7 +143,6 @@ IntelligenceWebClient.factory('PlayersFactory', [
             constructPositionDropdown: function(roster, rosterId, positions) {
 
                 angular.forEach(roster, function(player) {
-                    console.log(player);
                     //constructs position dropdown
                     player.selectedPositions = {};
 
