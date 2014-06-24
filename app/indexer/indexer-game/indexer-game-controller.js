c7b8c62cfc900c7768e703276a944c6c0478403b
Jason Morganson <jason@krossover.com>
2014-06-24 10:06:39 -0400

    Update indexer game to use data service

--- app/indexer/indexer-game/indexer-game-controller.js
+++ app/indexer/indexer-game/indexer-game-controller.js
@@ -15,58 +15,31 @@ var Game = angular.module('indexer-game');
  * @type {Controller}
  */
 Game.controller('indexer-game.Controller', [
-    '$scope', '$state', '$stateParams', '$modal', 'GAME_STATUSES', 'GAME_STATUS_IDS', 'GAME_TYPES', 'GAME_NOTE_TYPES', 'SessionService', 'AlertsService', 'UsersFactory', 'GamesFactory', 'SchoolsFactory', 'TeamsFactory', 'SportsFactory', 'LeaguesFactory', 'RawFilm.Modal',
-    function controller($scope, $state, $stateParams, $modal, GAME_STATUSES, GAME_STATUS_IDS, GAME_TYPES, GAME_NOTE_TYPES, session, alerts, users, games, schools, teams, sports, leagues, RawFilmModal) {
+    '$scope', '$state', '$stateParams', '$modal', 'GAME_STATUSES', 'GAME_STATUS_IDS', 'GAME_TYPES', 'GAME_NOTE_TYPES', 'SessionService', 'AlertsService', 'RawFilm.Modal', 'Indexer.Game.Data',
+    function controller($scope, $state, $stateParams, $modal, GAME_STATUSES, GAME_STATUS_IDS, GAME_TYPES, GAME_NOTE_TYPES, session, alerts, RawFilmModal, data) {
 
         $scope.GAME_TYPES = GAME_TYPES;
         $scope.GAME_STATUSES = GAME_STATUSES;
         $scope.GAME_STATUS_IDS = GAME_STATUS_IDS;
         $scope.GAME_NOTE_TYPES = GAME_NOTE_TYPES;
 
+        $scope.RawFilmModal = RawFilmModal;
+
         var gameId = $stateParams.id;
 
-        games.get(gameId, function(game) {
+        $scope.game = data.games.get(gameId);
+        console.log($scope.game);
+        $scope.team = data.teams.get($scope.game.teamId);
+        $scope.opposingTeam = data.teams.get($scope.game.opposingTeamId);
+        var league = data.leagues.get($scope.team.leagueId);
+        $scope.sport = data.sports.get(league.sportId);
+        $scope.school = data.schools.get($scope.team.schoolId);
 
-            $scope.game = game;
+        var headCoachRole = $scope.team.getHeadCoachRole();
 
-            var status = game.getStatus();
+        if (headCoachRole) {
 
-            alerts.add({
-                type: status.type,
-                message: status.name
-            });
-
-            teams.get(game.teamId, function(team) {
-
-                $scope.teamName = team.name;
-
-                leagues.get(team.leagueId, function(league) {
-
-                    $scope.sport = sports.get(league.sportId);
-                });
-
-                schools.get(team.schoolId, function(school) {
-
-                    $scope.school = school;
-                });
-
-                var headCoachRole = team.getHeadCoachRole();
-
-                if (headCoachRole) {
-
-                    users.get(headCoachRole.userId, function(user) {
-
-                        $scope.headCoach = user;
-                    });
-                }
-            });
-
-            teams.get(game.opposingTeamId, function(team) {
-
-                $scope.opposingTeamName = team.name;
-            });
-        });
-
-        $scope.RawFilmModal = RawFilmModal;
+            $scope.headCoach = data.users.get(headCoachRole.userId);
+        }
     }
 ]);
