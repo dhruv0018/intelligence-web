77ec6203e717d0c42f8630e2be7c381d631ac6ac
Jason Morganson <jason@krossover.com>
2014-07-03 22:19:19 -0400

    Use pointer events instead of click handler

--- src/directives/anchor.js
+++ src/directives/anchor.js
@@ -21,64 +21,55 @@ IntelligenceWebClient.directive('a', [
         function link($scope, element, attributes, controller) {
 
             var disabled = element.hasClass('disabled');
+            var pointerEvents = element.css('pointer-events');
 
             /* Disable buttons on HTTP requests */
             $rootScope.$on('httpRequest', function() {
 
-                /* Prevent clicks. */
-                element.on('click', function(event) {
-
-                    event.preventDefault();
-                });
-
                 element.addClass('disabled');
+                element.css('pointer-events', 'none');
             });
 
             /* Reset disabled button state on HTTP request error */
             $rootScope.$on('httpRequestError', function() {
 
-                element.off('click');
                 element.toggleClass('disabled', disabled);
+                element.css('pointer-events', pointerEvents);
             });
 
             /* Reset disabled button state on HTTP response */
             $rootScope.$on('httpResponse', function() {
 
-                element.off('click');
                 element.toggleClass('disabled', disabled);
+                element.css('pointer-events', pointerEvents);
             });
 
             /* Reset disabled button state on HTTP response error */
             $rootScope.$on('httpResponseError', function() {
 
-                element.off('click');
                 element.toggleClass('disabled', disabled);
+                element.css('pointer-events', pointerEvents);
             });
 
             /* Disable buttons on start of state change */
             $rootScope.$on('$stateChangeStart', function() {
 
-                /* Prevent clicks. */
-                element.on('click', function(event) {
-
-                    event.preventDefault();
-                });
-
                 element.addClass('disabled');
+                element.css('pointer-events', 'none');
             });
 
             /* Reset disabled button state on state change success */
             $rootScope.$on('$stateChangeSuccess', function() {
 
-                element.off('click');
                 element.toggleClass('disabled', disabled);
+                element.css('pointer-events', pointerEvents);
             });
 
             /* Reset disabled button state on state change error */
             $rootScope.$on('$stateChangeError', function() {
 
-                element.off('click');
                 element.toggleClass('disabled', disabled);
+                element.css('pointer-events', pointerEvents);
             });
         }
 
