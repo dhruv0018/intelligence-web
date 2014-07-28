929ece4e5008e8697bc196063b7a3d13fdce1d2d
Jason Morganson <jason@krossover.com>
2014-07-28 10:21:11 -0400

    Remove data leftovers

--- app/indexing/sidebar-playlist-controller.js
+++ app/indexing/sidebar-playlist-controller.js
@@ -17,7 +17,6 @@ Indexing.controller('Indexing.Sidebar.Playlist.Controller', [
     '$scope', 'IndexingService', 'PlayManager', 'EventManager', 'Indexing.Sidebar', 'Indexing.Data',
     function controller($scope, indexing, play, event, sidebar, data) {
 
-        $scope.data = data;
         $scope.game = data.game;
         $scope.play = play;
         $scope.event = event;
