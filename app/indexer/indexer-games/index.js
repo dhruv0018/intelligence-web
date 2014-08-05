d88cb702806fd2d036347e3eaf97df9041a3e915
Tory Mance <tory@krossover.com>
2014-08-05 16:19:05 -0400

    Removed redundant variables

--- app/indexer/indexer-games/index.js
+++ app/indexer/indexer-games/index.js
@@ -97,14 +97,13 @@ Games.controller('indexer-games.Controller', [
 
         $scope.userId = session.currentUser.id;
 
-        var role = session.currentUser.currentRole;
-        $scope.userLocation = role.indexerGroupId;
+        var userLocation = session.currentUser.currentRole.indexerGroupId;
 
-        if ($scope.userLocation === INDEXER_GROUPS.US_MARKETPLACE) {
+        if (userLocation === INDEXER_GROUPS.US_MARKETPLACE) {
             $scope.signUpLocation = config.links.indexerSignUp.unitedStates.uri;
-        } else if ($scope.userLocation === INDEXER_GROUPS.INDIA_MARKETPLACE || $scope.userLocation === INDEXER_GROUPS.INDIA_OFFICE) {
+        } else if (userLocation === INDEXER_GROUPS.INDIA_MARKETPLACE || $scope.userLocation === INDEXER_GROUPS.INDIA_OFFICE) {
             $scope.signUpLocation = config.links.indexerSignUp.india.uri;
-        } else if ($scope.userLocation === INDEXER_GROUPS.PHILIPPINES_OFFICE) {
+        } else if (userLocation === INDEXER_GROUPS.PHILIPPINES_OFFICE) {
             $scope.userLocation = config.links.indexerSignUp.philippines.uri;
         }
 
