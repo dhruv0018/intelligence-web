3eec956921852f996c04d8407c0a941e6088eed3
Wes Pickett <wes.pickett@gmail.com>
2014-08-18 18:48:10 -0400

    reel-play directive

--- app/reels/index.js
+++ app/reels/index.js
@@ -31,15 +31,39 @@ ReelsArea.config([
         $stateProvider
 
         .state('ReelsArea', {
-            url: '/reel',
+            url: '/reel/:id',
             parent: 'base',
             views: {
                 'main@root': {
                     templateUrl: 'reels/template.html',
                     controller: 'ReelsArea.controller'
                 }
+            },
+            resolve: {
+                'Reels.Data': [
+                    '$q', 'Reels.Data.Dependencies',
+                    function($q, data) {
+                        return $q.all(data);
+                    }
+                ]
             }
         });
+    }
+]);
+
+ReelsArea.service('Reels.Data.Dependencies', [
+    'GamesFactory', 'PlaysFactory', 'TeamsFactory',
+    function dataService(games, plays, teams) {
+
+        var Data = {};
+
+        angular.forEach(arguments, function(arg) {
+
+            Data[arg.description] = arg.load();
+        });
+
+        return Data;
+
     }
 ]);
 
@@ -50,9 +74,33 @@ ReelsArea.config([
  * @type {Controller}
  */
 ReelsArea.controller('ReelsArea.controller', [
-    '$scope', '$state', '$stateParams',
-    function controller($scope, $state, $stateParams) {
-        console.log($scope, $state, $stateParams);
+    '$scope', '$state', '$stateParams', 'Reels.Data',
+    function controller($scope, $state, $stateParams, data) {
+
+        $scope.getHomeTeam = function(playId) {
+
+            var gameId = data.plays.get(playId).gameId;
+            var teamId = data.games.get(gameId).teamId;
+
+            return data.teams.get(teamId);
+        };
+
+        $scope.getOpposingTeam = function(playId) {
+
+            var gameId = data.plays.get(playId).gameId;
+            var teamId = data.games.get(gameId).opposingTeamId;
+
+            return data.teams.get(teamId);
+        };
+
+        $scope.getDatePlayed = function(playId) {
+            var gameId = data.plays.get(playId).gameId;
+
+            return data.games.get(gameId).datePlayed;
+        };
+
+        $scope.data = data;
+        //$scope.reels = {"id":1,"name":"Highschool","uploaderUserId":1,"uploaderTeamId":1,"createdAt":"-0001-11-30T00:00:00+00:00","updatedAt":"-0001-11-30T00:00:00+00:00","plays":[4,3,11,12]};
     }
 ]);
 
