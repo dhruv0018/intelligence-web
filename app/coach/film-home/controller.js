379e090a46c76ad86027852630332be9ab97c151
Jason Morganson <jason@krossover.com>
2014-08-20 09:09:52 -0400

    Use factories directly

--- app/coach/film-home/controller.js
+++ app/coach/film-home/controller.js
@@ -14,14 +14,16 @@ var FilmHome = angular.module('Coach.FilmHome');
  * @type {controller}
  */
 FilmHome.controller('Coach.FilmHome.controller', [
-    '$rootScope', '$scope', '$state', '$filter', 'GamesFactory', 'PlayersFactory', 'SessionService', 'Coach.Data', 'Coach.FilmHome.GameFilters',
-    function controller($rootScope, $scope, $state, $filter, games, players, session,  data, filtersData) {
+    '$rootScope', '$scope', '$state', '$filter', 'TeamsFactory', 'GamesFactory', 'PlayersFactory', 'SessionService', 'Coach.Data', 'Coach.FilmHome.GameFilters',
+    function controller($rootScope, $scope, $state, $filter, teams, games, players, session,  data, filtersData) {
+
+        var teamId = session.currentUser.currentRole.teamId;
 
         $scope.playersList = data.playersList;
-        $scope.games = data.games.getCollection();
-        $scope.gamesList = data.games.getList();
-        $scope.teams = data.teams.getCollection();
-        $scope.team = $scope.teams[session.currentUser.currentRole.teamId];
+        $scope.games = games.getCollection();
+        $scope.gamesList = games.getList();
+        $scope.teams = teams.getCollection();
+        $scope.team = teams.get(teamId);
         $scope.roster = $scope.team.roster;
         $scope.activeRoster = players.constructActiveRoster($scope.playersList, $scope.roster.id);
         $scope.query = '';
