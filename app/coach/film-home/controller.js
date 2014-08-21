09df97a131dcae8bba135596afb517e96483ae5c
Michael <michael@krossover.com>
2014-08-21 17:17:14 -0400

    componentized filters

--- app/coach/film-home/controller.js
+++ app/coach/film-home/controller.js
@@ -17,8 +17,8 @@ var FilmHome = angular.module('Coach.FilmHome');
  * @type {controller}
  */
 FilmHome.controller('Coach.FilmHome.controller', [
-    '$rootScope', '$scope', '$state', '$filter', 'GamesFactory', 'PlayersFactory', 'SessionService', 'Coach.Data', 'Coach.FilmHome.GameFilters',
-    function controller($rootScope, $scope, $state, $filter, games, players, session,  data, filtersData) {
+    '$rootScope', '$scope', '$state', '$filter', 'GamesFactory', 'PlayersFactory', 'ReelsFactory', 'SessionService', 'Coach.Data',
+    function controller($rootScope, $scope, $state, $filter, games, players, reels, session, data) {
         $scope.playersList = data.playersList;
         $scope.games = data.games.getCollection();
         $scope.gamesList = data.games.getList();
@@ -27,23 +27,6 @@ FilmHome.controller('Coach.FilmHome.controller', [
         $scope.roster = $scope.team.roster;
         $scope.activeRoster = players.constructActiveRoster($scope.playersList, $scope.roster.id);
         $scope.query = '';
-        $scope.filters = filtersData.filters;
-
-//        $scope.$watch('filters.all', function(all) {
-//
-//            if (all === true) {
-//                filtersData.disableOthers();
-//            }
-//
-//            else if (filtersData.othersDisabled === true) {
-//                $scope.filters.all = true;
-//            }
-//        });
-//
-//        $scope.$watchCollection('filters.others', function(others) {
-//            filtersData.watchOthers();
-//        });
-
     }
 ]);
 
