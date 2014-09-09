518bb35a2dde102dd62eead3bdd31ec0cf86c8de
Wes Pickett <wes.pickett@gmail.com>
2014-09-09 17:24:28 -0400

    sharing

--- src/factories/reels.js
+++ src/factories/reels.js
@@ -33,6 +33,48 @@ IntelligenceWebClient.factory('ReelsFactory', [
 
             updateDate: function() {
                 this.updatedAt = moment.utc();
+            },
+            share: function share(toUserIds, reelId) {
+
+                var self = this;
+                var shareTime = new Date();
+
+                self.shares = self.shares || [];
+
+                reelId = (Number(reelId) >= 0) ? reelId : self.id;
+
+                toUserIds = toUserIds || [];
+
+                toUserIds.forEach(function(toUserId) {
+
+                    if (!Number(toUserId) || toUserId < 0) return;
+
+                    self.shares.push({
+                        userId: session.currentUser.id,
+                        reelId: reelId,
+                        sharedWithUserId: toUserId,
+                        /*sharedWithTeamId: undefined,*/
+                        createdAt: shareTime
+                    });
+                });
+
+                return self;
+            },
+            sharedBy: function sharedBy(sharedWithUserId) {
+                var self = this;
+
+                userId = Number(sharedWithUserId);
+
+                if (!isNaN(userId)) {
+
+                    //game has shares
+                    if (self.sharedWithLookupTable) {
+                        return (self.sharedWithLookupTable[userId]) ? self.sharedWithLookupTable[userId].userId : undefined;
+                    }
+                }
+            },
+            isSharedWith: function isSharedWith(userId) {
+                return Number(sharedBy(userId)) >= 0;
             }
         };
 
