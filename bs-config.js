ce186064d976a18677545fa9c7cc767c75381524
Bryan Gislason <bryang@krossover.com>
2014-09-22 16:31:30 -0400

    browsersync open browser set to false

--- bs-config.js
+++ bs-config.js
@@ -90,7 +90,7 @@ module.exports = {
      |--------------------------------------------------------------------------
      | https://github.com/shakyShane/browser-sync/wiki/options#wiki-open
      */
-    open: true,
+    open: false,
 
     /*
      |--------------------------------------------------------------------------
