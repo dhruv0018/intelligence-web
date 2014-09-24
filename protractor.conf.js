aafd21c08cd0b730fbb4824edcbc16c301e7f429
Jason Morganson <jason@krossover.com>
2014-09-24 15:47:21 -0400

    Tweak to get codeship to build

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,24 +1,22 @@
 var environment = process.env.NODE_ENV;
-var baseUrl;
 
-baseUrl = 'http://localhost:8000/intelligence/';
+var baseUrl = 'http://localhost:8000/intelligence/';
 
 if (environment === 'development') {
-  
+
     baseUrl = 'http://localhost:8000/intelligence/';
 
-} 
+}
 
 else if (environment === 'qa') {
 
     baseUrl = 'http://v2-qa.krossover.com/intelligence/';
 
-} 
+}
 
 else if (environment === 'uat') {
 
     baseUrl = 'http://v2-uat.krossover.com/intelligence/';
-
 }
 
 exports.config = {
