ec8274abb6308157df20ac423e3cbd8c6faa4b3d
Tory Mance <tory@krossover.com>
2014-11-10 11:06:56 -0500

    Changed variable names, used constants

--- app/coach/analytics/index.js
+++ app/coach/analytics/index.js
@@ -52,29 +52,34 @@ Analytics.config([
  */
 
 Analytics.controller('AnalyticsController', [
-    '$scope', '$state', '$stateParams', 'SessionService', 'LeaguesFactory', 'TeamsFactory',
-    function controller($scope, $state, $stateParams, session, leagues, teams) {
+    '$scope', '$state', '$stateParams', 'SessionService', 'LeaguesFactory', 'TeamsFactory', 'GAME_TYPES',
+    function controller($scope, $state, $stateParams, session, leagues, teams, GAME_TYPES) {
         var teamId = session.currentUser.currentRole.teamId;
         var team = teams.get(teamId);
         var league = leagues.get(team.leagueId);
         $scope.seasons = league.seasons;
         $scope.loadingTables = true;
-        $scope.data = {};
+        $scope.statsData = {};
+
+        //Game type constants
+        $scope.CONFERENCE = GAME_TYPES.CONFERENCE;
+        $scope.NON_CONFERENCE = GAME_TYPES.NON_CONFERENCE;
+        $scope.PLAYOFF = GAME_TYPES.PLAYOFF;
 
         $scope.filterQuery = {
             seasonId: league.seasons[0].id,
             gameType: ''
         };
 
-        team.generateStats($scope.filterQuery).then(function(data) {
-            $scope.data = data;
+        team.generateStats($scope.filterQuery).then(function(statsData) {
+            $scope.statsData = statsData;
             $scope.loadingTables = false;
         });
 
         $scope.generateStats = function() {
             $scope.loadingTables = true;
-            team.generateStats($scope.filterQuery).then(function(data) {
-                $scope.data = data;
+            team.generateStats($scope.filterQuery).then(function(statsData) {
+                $scope.statsData = statsData;
                 $scope.loadingTables = false;
             });
         };
