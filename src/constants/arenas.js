3cec30f0855af69f3d76959a5bd170bfb8a64ef9
Alex Young <alexyoung1992@gmail.com>
2014-11-20 12:59:47 -0500

    Added arena id as a constant

--- src/constants/arenas.js
+++ src/constants/arenas.js
@@ -8,31 +8,37 @@ var IntelligenceWebClient = angular.module(pkg.name);
 var ARENA_TYPES = {
 
     1: {
+        id: 1,
         name: 'High School Basketball',
         svg: 'hs-arena'
     },
 
     2: {
+        id: 2,
         name: 'NCAA Basketball',
         svg: 'ncaa-arena'
     },
 
     3: {
+        id: 3,
         name: 'NBA Basketball',
         svg: 'nba-arena'
     },
 
     4: {
+        id: 4,
         name: 'FIBA Basketball',
         svg: 'fiba-arena'
     },
 
     5: {
+        id: 5,
         name: 'Men\'s Outdoor Lacrosse',
         svg: 'mens-outdoor-lax-arena'
     },
 
     6: {
+        id: 6,
         name: 'Women\'s Outdoor Lacrosse',
         svg: 'womens-outdoor-lax-arena'
     }
