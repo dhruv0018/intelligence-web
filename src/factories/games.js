9f8b79258994ca47b4a6db9fb9df7cebc0f8abb6
Wes Pickett <wes.pickett@gmail.com>
2014-12-10 19:23:19 -0500

    fix merge conflict

--- src/factories/games.js
+++ src/factories/games.js
@@ -100,6 +100,7 @@ IntelligenceWebClient.factory('GamesFactory', [
 
                     return game.isSharedWithUser(user);
                 });
+            },
 
             saveNotes: function() {
 
