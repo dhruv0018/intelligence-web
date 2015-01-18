8e0e0663c33278cebfe838dce54bc9a467a88edc
Jason Morganson <jason@krossover.com>
2015-01-17 20:30:04 -0500

    Add game schema

---
+++ schemas/video.js
@@ -0,0 +1,27 @@
+{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "Video Schema",
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "guid": {
+            "type": "string"
+        },
+        "status": {
+            "type": "integer"
+        },
+        "videoTranscodeProfiles": {
+            "type": "array",
+            "items": { "$ref": "transcodeProfile" }
+        },
+        "duration": {
+            "type": "integer"
+        },
+        "thumbnail": {
+            "type": "string"
+        }
+    },
+    "additionalProperties": false
+}
