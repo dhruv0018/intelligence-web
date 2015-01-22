90dd3799b3c24cd0fa61c9999e6a47f3864311f1
Jason Morganson <jason@krossover.com>
2015-01-22 09:21:22 -0500

    Handle deleting the database

--- src/managers/storage.js
+++ src/managers/storage.js
@@ -10,12 +10,28 @@ var IntelligenceWebClient = angular.module(pkg.name);
  * @type {service}
  */
 IntelligenceWebClient.service('StorageManager', [
-    'RootStorage',
-    function service(root) {
+    '$log', 'RootStorage',
+    function service($log, root) {
 
         this.clear = function() {
 
             root = Object.create(null);
+
+            var request = indexedDB.deleteDatabase(pkg.name);
+
+            request.onsuccess = function() {
+
+                $log.info('Database deleted');
+
+                window.location.reload();
+            };
+
+            request.onerror = function() {
+
+                $log.error('Database could not be deleted');
+
+                throw new Error('Database could not be deleted');
+            };
         };
     }
 ]);
