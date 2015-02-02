93255ab3a9218dc862e7aeffad1a40fdf52c5266
Alex Young <alexyoung1992@gmail.com>
2015-02-02 03:04:57 -0500

    Scaffolding Athlete.Profile.Highlights module

---
+++ app/athlete/profile/highlights/controller.js
@@ -0,0 +1,21 @@
+/* Fetch angular from the browser scope */
+var angular = window.angular;
+
+/**
+ * Profile.Highlights page module.
+ * @module Profile.Highlights
+ */
+var Highlights = angular.module('Athlete.Profile.Highlights');
+
+/**
+ * Profile.Highlights controller.
+ * @module Profile.Highlights
+ * @name Profile.Highlights.controller
+ * @type {controller}
+ */
+Highlights.controller('Athlete.Profile.Highlights.controller', [
+    '$scope',
+    function controller($scope) {
+
+    }
+]);
