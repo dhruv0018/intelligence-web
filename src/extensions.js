27e0e89342d8fa5146686b5b7a5c769cb9a3ccbb
Bryan Gislason <bryang@krossover.com>
2015-02-06 11:37:47 -0500

    add inheritPrototype for better inheritance

--- src/extensions.js
+++ src/extensions.js
@@ -6,6 +6,19 @@ function inherit(parent, extra) {
     return angular.extend(new (angular.extend(function() {}, {prototype: parent}))(), extra);
 }
 
+function inheritPrototype(childObject, parentObject) {
+    // Copy the properties and methods from the parentObject onto the childObject​
+    // so the copyOfParent object now has everything the parentObject has ​
+    var copyOfParent = Object.create(parentObject.prototype);
+
+    // Then we set the constructor of this new object to point to the childObject.​
+    copyOfParent.constructor = childObject;
+
+    // Then we set the childObject prototype to copyOfParent,
+    // so that the childObject can in turn inherit everything from copyOfParent (from parentObject)​
+    childObject.prototype = copyOfParent;
+}
+
 function augment(dst) {
 
     angular.forEach(arguments, function(obj) {
@@ -24,6 +37,7 @@ function augment(dst) {
 
 angular.extend(angular, {
     'inherit': inherit,
-    'augment': augment
+    'augment': augment,
+    'inheritPrototype': inheritPrototype
 });
 
