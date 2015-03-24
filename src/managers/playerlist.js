c53434a7e48520f8c465cf7fd92ab9786b547dde
Michael <michaels3491@gmail.com>
2015-03-24 11:20:35 -0400

    extending player instead of setting individual options

--- src/managers/playerlist.js
+++ src/managers/playerlist.js
@@ -21,15 +21,12 @@ IntelligenceWebClient.service('PlayerlistManager', [
                     game.rosters[game.opposingTeamId].playerInfo);
 
                 Object.keys(mergedRosters).forEach(function(playerId) {
-                    var player = players.get(playerId);
                     var playerInfo = mergedRosters[playerId];
-                    playerInfo.playerId = parseInt(playerId);
-                    playerInfo.jerseyNumber = (playerInfo.jerseyNumber.length === 0) ? 'UN' : parseInt(playerInfo.jerseyNumber, 10);
-                    playerInfo.primaryJerseyColor = game.rosters[game.teamId].playerInfo[playerId] ? game.primaryJerseyColor : game.opposingPrimaryJerseyColor;
-                    playerInfo.firstName = player.firstName;
-                    playerInfo.lastName = player.lastName;
-                    playerInfo.type = 'Player';
-                    playerList.push(playerInfo);
+                    var player = angular.extend({}, players.get(playerId), playerInfo);
+                    player.jerseyNumber = (playerInfo.jerseyNumber.length === 0) ? 'UN' : parseInt(playerInfo.jerseyNumber, 10);
+                    player.primaryJerseyColor = game.rosters[game.teamId].playerInfo[playerId] ? game.primaryJerseyColor : game.opposingPrimaryJerseyColor;
+                    player.type = 'Player';
+                    playerList.push(player);
                 });
             },
             get: function() {
