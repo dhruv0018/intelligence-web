d40291ddb90b69bea0935cd7b603b55c6efb7590
Jason Morganson <jason@krossover.com>
2015-03-25 11:12:14 -0400

    Remove register scope methods

--- src/managers/plays.js
+++ src/managers/plays.js
@@ -18,11 +18,6 @@ IntelligenceWebClient.service('PlaysManager', [
 
         this.plays = [];
 
-        //Index of scopes assosicated with the plays
-        //indexed by 'id' for game breakdown playlist, and
-        //by '$$hashkey' for indexing playlist
-        this.playScopes = {};
-
         /**
          * Resets the plays.
          * @param {Array} plays - array to set the plays to.
@@ -66,34 +61,6 @@ IntelligenceWebClient.service('PlaysManager', [
         this.getNextPlay = function(currentPlay) {
             var index = this.plays.indexOf(currentPlay);
             return (++index < this.plays.length) ? this.plays[index] : null;
-        };
-
-        this.registerPlayScope = function registerPlayScope(playScope) {
-            //create hash of play scopes indexed by the scopes play's id
-            var registeredId;
-            if (playScope.play.id) {
-                registeredId = playScope.play.id;
-            } else {
-                //Special case for indexing. Automatically selects play
-                //to keep the current play at the top of the playlist
-                registeredId = playScope.play.$$hashKey;
-                if (typeof playScope.selectPlay === 'function') playScope.selectPlay();
-            }
-            this.playScopes[registeredId] = playScope;
-        };
-
-        this.getNextPlayScope = function getNextPlayScope(currentPlay) {
-            var currentPlayIndex = this.plays.indexOf(currentPlay);
-            var nextPlay = this.plays[(currentPlayIndex + 1) % this.plays.length];
-            if (nextPlay) {
-
-                if (angular.isUndefined(nextPlay.isFiltered) || nextPlay.isFiltered) {
-                    //Find the next visible play
-                    return this.playScopes[nextPlay.id];
-                } else {
-                    return this.getNextPlayScope(nextPlay);
-                }
-            }
         };
 
         /**
