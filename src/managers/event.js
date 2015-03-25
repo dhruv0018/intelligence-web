0d3a73e7f96814a437a7170c5afd62ab79d04463
Jason Morganson <jason@krossover.com>
2015-03-25 19:36:16 -0400

    Remove event methods

--- src/managers/event.js
+++ src/managers/event.js
@@ -23,74 +23,6 @@ IntelligenceWebClient.service('EventManager', [
         this.current = null;
 
         /**
-         * Checks whether the event is a floating event.
-         * @returns - true if the event is floating event; false otherwise.
-         */
-        this.isFloatingEvent = function(event) {
-
-            event = event || this.current;
-
-            /* If there is no current event or the event hasn't been created
-             * then its can not be a floating event. */
-            if (!event || !event.tagId) return false;
-
-            /* Check if the given event is an end tag. */
-            return this.tagset.isFloatTag(event.tagId);
-        };
-
-        /**
-         * Checks whether the event is an ending event.
-         * @returns - true if the event is an end event; false otherwise.
-         */
-        this.isEndEvent = function(event) {
-
-            event = event || this.current;
-
-            /* If there is no current event or the event hasn't been created
-             * then its can not be a end event. */
-            if (!event || !event.tagId) return false;
-
-            /* Check if the given event is an end tag. */
-            return this.tagset.isEndTag(event.tagId);
-        };
-
-        /**
-         * Checks whether the event is an end-and-start event.
-         * @returns - true if the event is an end-and-start event; false otherwise.
-         */
-        this.isEndAndStartEvent = function(event) {
-
-            event = event || this.current;
-
-            /* If there is no current event or the event hasn't been created
-             * then its can not be a end event. */
-            if (!event || !event.tagId) return false;
-
-            var tagId = event.tagId;
-            var tag = this.tagset.tags[tagId];
-
-            /* Check if the given event is an end tag and only has one child. */
-            return this.isEndEvent(event) && tag.children.length === 1;
-        };
-
-        /**
-         * Checks whether the event has variables.
-         * @returns - true if the event has variables; false otherwise.
-         */
-        this.hasVariables = function() {
-
-            /* If there is no current event or the event hasn't been created
-             * there are no variables in the event yet. */
-            if (!this.current || !this.current.tagId) return false;
-
-            var tagId = this.current.tagId;
-            var tag = this.tagset.tags[tagId];
-
-            /* Check if the tag has tag variables. */
-            return !!Object.keys(tag.tagVariables).length;
-        };
-
-        /**
          * Gets the value of the active variable.
          * @returns - the value of the active variable.
          */
@@ -123,37 +55,6 @@ IntelligenceWebClient.service('EventManager', [
             var tagVariable = tagVariables[index];
 
             this.current.variableValues[tagVariable.id].value = undefined;
-        };
-
-        /**
-         * Checks if all the variables have values.
-         * @returns {Boolean} true, if all of the variables have a value;
-         * false otherwise.
-         */
-        this.allEventVariablesHaveValues = function() {
-
-            var self = this;
-
-            var tagId = this.current.tagId;
-            var tag = this.tagset.tags[tagId];
-            var tagVariables = tag.tagVariables;
-            var variableValues = self.current.variableValues;
-
-            /* Ensure that every required variable has a value. */
-            return Object.keys(tagVariables).every(function(index) {
-
-                /* Lookup the tag variable by its script index. */
-                var tagVariable = tagVariables[index];
-
-                /* If the variable is not required, it doesn't need a value. */
-                if (!tagVariable.isRequired) return true;
-
-                /* Lookup the variable by tag variable ID. */
-                var variable = variableValues[tagVariable.id];
-
-                /* Check if the variable has a value. */
-                return !!variable.value;
-            });
         };
 
         /**
