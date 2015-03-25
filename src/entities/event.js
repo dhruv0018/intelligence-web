4c7ce7d7932363621c5c61f9090100d1dfbbd172
Jason Morganson <jason@krossover.com>
2015-03-25 12:12:03 -0400

    Add tag methods to the event entity

--- src/entities/event.js
+++ src/entities/event.js
@@ -21,6 +21,65 @@ class Event extends Entity {
 
         Object.assign(this, event, tag, { time });
     }
+
+    /**
+     * Checks whether the event has variables.
+     * @returns - true if the event has variables; false otherwise.
+     */
+    get hasVariables () {
+
+        /* Check if the event has tag variables. */
+        return !!Object.keys(this.tagVariables).length;
+    }
+
+    /**
+     * Checks if all the variables have values.
+     * @returns {Boolean} true, if all of the variables have a value;
+     * false otherwise.
+     */
+    get isValid () {
+
+        function variableFromKey (tagVariableId) {
+
+            /* Lookup the tag variable. */
+            return this.variableValues[tagVariableId];
+        }
+
+        function variableIsValid (variable) {
+
+            /* If the variable is not required, it doesn't need a value. */
+            if (!variable.isRequired) return true;
+
+            /* Check if the variable has a value. */
+            return !!variable.value;
+        }
+
+        /* Ensure that every required variable has a value. */
+        return Object.keys(this.variableValues)
+        .map(variableFromKey)
+        .every(variableIsValid);
+    }
+
+    /**
+     * Checks whether the event is a floating event.
+     * @returns - true if the event is floating event; false otherwise.
+     */
+    get isFloat () {
+
+        return this.isStart === false &&
+               this.isEnd === false &&
+               this.children.length === 0;
+    }
+
+    /**
+     * Checks whether the event is an end-and-start event.
+     * @returns - true if the event is an end-and-start event; false otherwise.
+     */
+    get isEndAndStart () {
+
+        /* Check if the given event is an end tag and only has one child. */
+        return this.isEnd && this.children.length === 1;
+    }
 }
 
 export default Event;
