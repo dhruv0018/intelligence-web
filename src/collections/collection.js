55922d414674a1cf6357147f0d47e5c75fbc22cb
Alex Young <alexyoung1992@gmail.com>
2015-03-30 01:02:58 -0400

    Cleaned up Collection definition

--- src/collections/collection.js
+++ src/collections/collection.js
@@ -1,10 +1,23 @@
 class Collection {
 
+    /**
+     * Constructor:
+     * Enforce interface by throwing error on new
+     *
+     * @return {Error} Cannot instantiate
+     */
     constructor() {
 
         throw new Error('Cannot instantiate the Collection interface');
     }
 
+    /**
+     * Method:extend
+     * Replicates a given collection
+     *
+     * @param: {Object} Collection to replicate
+     * @return: {Object} New collection
+     */
     extend(collection) {
 
         Object.assign(this, collection);
@@ -19,8 +32,6 @@ class Collection {
     add(item) {}
 
     remove(item) {}
-
-    sort(property) {}
 }
 
 export default Collection;
