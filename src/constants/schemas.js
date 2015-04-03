9b36078415324326119167ffdc6a418edcb6015a
Alex Young <alexyoung1992@gmail.com>
2015-04-02 23:45:10 -0400

    Added Subscription schema to constants

--- src/constants/schemas.js
+++ src/constants/schemas.js
@@ -10,3 +10,4 @@ IntelligenceWebClient.constant('TEAM_SCHEMA', require('../../schemas/team.json')
 IntelligenceWebClient.constant('GAME_SCHEMA', require('../../schemas/game.json'));
 IntelligenceWebClient.constant('REEL_SCHEMA', require('../../schemas/reel.json'));
 IntelligenceWebClient.constant('LEAGUE_SCHEMA', require('../../schemas/league.json'));
+IntelligenceWebClient.constant('SUBSCRIPTION_SCHEMA', require('../../schemas/subscription.json'));
