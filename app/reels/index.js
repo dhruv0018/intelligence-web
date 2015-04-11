b350004a97ca1160a2edca37f75fa536f70fb9f6
Jason Morganson <jason@krossover.com>
2015-04-11 12:37:42 -0400

    Testing new resolve class

--- app/reels/index.js
+++ app/reels/index.js
@@ -19,6 +19,34 @@ ReelsArea.run([
     }
 ]);
 
+
+class ReelsData {
+
+    constructor (reelId) {
+
+        /* Get the angular injector for the document. */
+        const $injector = angular.element(document).injector();
+
+        /* Get dependencies. */
+        const reels = $injector.get('ReelsFactory');
+        const games = $injector.get('GamesFactory');
+        const teams = $injector.get('TeamsFactory');
+        const plays = $injector.get('PlaysFactory');
+        const players = $injector.get('PlayersFactory');
+        const leagues = $injector.get('LeaguesFactory');
+        const tagsets = $injector.get('TagsetsFactory');
+
+        /* Load data. */
+        this.tagset = tagsets.load();
+        this.leagues = leagues.load();
+        this.reel = reels.load(reelId);
+        this.games = games.load({reelId: reelId});
+        this.teams = teams.load({reelId: reelId});
+        this.plays = plays.load({reelId: reelId});
+        this.players = players.load({reelId: reelId});
+    }
+}
+
 /**
  * ReelsArea page state router.
  * @module ReelsArea
@@ -53,10 +81,13 @@ ReelsArea.config([
             },
             resolve: {
                 'Reels.Data': [
-                    '$q', '$state', '$stateParams', 'Reels.Data.Dependencies',
-                    function dataService($q, $state, $stateParams, data) {
+                    '$q', '$stateParams',
+                    function dataService($q, $stateParams) {
 
-                        return $q.all(data($stateParams).load());
+                        let reelId = Number($stateParams.id);
+                        let data = new ReelsData(reelId);
+                        console.log(data);
+                        return $q.all(data);
                     }
                 ]
             },
