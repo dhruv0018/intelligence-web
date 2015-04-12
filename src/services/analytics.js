6a89bdb7866bfd169fd34b300d5ab0f69c2f1589
Dave Krstin <david@krossover.com>
2015-04-11 22:55:12 -0400

    Added  call for eventTrack in the track method. Also, added documentation for service methods and cleaned up the code.

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -18,14 +18,17 @@ IntelligenceWebClient.service('AnalyticsService', [
     'SessionService', 'TeamsFactory', 'SportsFactory', 'LeaguesFactory', '$analytics',
     function (session, teams, sports, leagues, $analytics) {
 
-        console.log('Analytics Service working!');
-
+        console.log('$analytics', $analytics);
         return {
+
+            /**
+             * Identifies the current user (email, userID, roles, team,
+             * league, sport, and season) and reports to Mixpanel (via segment.io).
+             * @return undefined
+             */
             identify: function () {
 
-                console.log('Indentifying user...');
-                let user = session.retrieveCurrentUser();
-                console.log('Current User:', user);
+                let user     = session.retrieveCurrentUser();
                 let teamId   = user.currentRole.teamId || '';
                 let leagueId = '';
                 let sport    = '';
@@ -38,15 +41,37 @@ IntelligenceWebClient.service('AnalyticsService', [
                 }
 
                 $analytics.setUserProperties(user.id, {
-                    Email: user.email,
-                    UserID: user.id,
-                    WebVersion: pkg.version,
-                    AccountCreated: '', // need this!
-                    RoleName: user.currentRole.type.name,
-                    RoleTeam: teamId,
-                    LeagueID: leagueId,
-                    Sport: sport,
-                    CurrentSeason: '' // need this!
+                    Email          : user.email,
+                    UserID         : user.id,
+                    WebVersion     : pkg.version,
+                    AccountCreated : '', // TODO: need this!
+                    RoleName       : user.currentRole.type.name,
+                    RoleTeam       : teamId,
+                    LeagueID       : leagueId,
+                    Sport          : sport,
+                    CurrentSeason  : '' // TODO: need this!
+                });
+            },
+
+            /**
+             * Tracks the user event that has just occurred and reports
+             * to Mixpanel (via segment.io).
+             * @param {String} category  - The section of the application the event originated (e.g. 'Account', 'Breakdown').
+             * @param {String} action    - The action performed to initate the event (e.g. 'Selected', 'Filtered', etc.).
+             * @param {String} label     - The item or target of the event action.
+             * @param {String} pageInfo  - Info about the page.
+             * @param {String} property1 - Extra property.
+             * @param {String} property2 - Extra propert.
+             * @return undefined
+             */
+            track: function (category, action, label, pageInfo, property1, property2) {
+
+                $analytics.eventTrack(action, {
+                    'Category'  : category,
+                    'Label'     : label,
+                    'Page Info' : pageInfo || '',
+                    'Property'  : property1 || '',
+                    'Property2' : property2 || ''
                 });
             }
         };
