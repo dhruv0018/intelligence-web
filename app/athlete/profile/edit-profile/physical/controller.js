a6e593881db58f50346a632af12d9e745ded33af
Tory Mance <tory@krossover.com>
2015-04-20 13:59:00 -0400

    Add hometown and calculate minutes and seconds for mile times

--- app/athlete/profile/edit-profile/physical/controller.js
+++ app/athlete/profile/edit-profile/physical/controller.js
@@ -31,13 +31,41 @@ function PhysicalController (
 ) {
     $scope.athlete = session.getCurrentUser();
 
-    //display height as temporary feet and inches variables
-    $scope.athleteHeightFeet = Math.floor($scope.athlete.profile.height / 12);
-    $scope.athleteHeightInches = $scope.athlete.profile.height % 12;
+    //display height and wingspan as temporary feet and inches variables
+    $scope.athleteHeightFeet = getFeet($scope.athlete.profile.height);
+    $scope.athleteHeightInches = getInches($scope.athlete.profile.height);
+    $scope.athleteWingFeet = getFeet($scope.athlete.profile.wingspan);
+    $scope.athleteWingInches = getInches($scope.athlete.profile.wingspan);
+
+    //display mile times as temporary minutes and seconds variables
+    $scope.athleteOneMileTimeMinutes = getMinutes($scope.athlete.profile.oneMileTime);
+    $scope.athleteOneMileTimeSeconds = getSeconds($scope.athlete.profile.oneMileTime);
+    $scope.athleteThreeMileTimeMinutes = getMinutes($scope.athlete.profile.threeMileTime);
+    $scope.athleteThreeMileTimeSeconds = getSeconds($scope.athlete.profile.threeMileTime);
+
+    function getFeet(measurement) {
+        return Math.floor(measurement / 12);
+    }
+
+    function getInches(measurement) {
+        return measurement % 12;
+    }
+
+    function getMinutes(measurement) {
+        return Math.floor(measurement / 60);
+    }
+
+    function getSeconds(measurement) {
+        return measurement % 60;
+    }
 
     $scope.savePhysical = function savePhysical() {
         //recombine feet and inches before saving
         $scope.athlete.profile.height = Number($scope.athleteHeightFeet * 12) + Number($scope.athleteHeightInches);
+        $scope.athlete.profile.wingspan = Number($scope.athleteWingFeet * 12) + Number($scope.athleteWingInches);
+        //recombine minutes and seconds before saving
+        $scope.athlete.profile.oneMileTime = Number($scope.athleteOneMileTimeMinutes * 60) + Number($scope.athleteOneMileTimeSeconds);
+        $scope.athlete.profile.threeMileTime = Number($scope.athleteThreeMileTimeMinutes * 60) + Number($scope.athleteThreeMileTimeSeconds);
         $scope.athlete.save();
     };
 }
