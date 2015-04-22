4e7b9d8cef09a4580e7743bca1cef575b71c4ef1
Akash <akash@krossover.com>
2015-04-22 13:02:44 -0400

    Change game to reel

--- app/reel/restricted.html.js
+++ app/reel/restricted.html.js
@@ -2,7 +2,7 @@ export default `
 
 <div class="locked-game">
     <i class="icon icon-lock"></i>
-    <h3>You do not have permission to access this game</h3>
+    <h3>You do not have permission to access this reel</h3>
 </div>
 
 `;
