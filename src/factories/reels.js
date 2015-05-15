c850e6d320710eec01f89bc8b2c19af9de897754
Tory Mance <tory@krossover.com>
2015-05-15 16:11:16 -0400

    Remove modal from play and make adding play to reel loop one line

--- src/factories/reels.js
+++ src/factories/reels.js
@@ -159,11 +159,12 @@ IntelligenceWebClient.factory('ReelsFactory', [
             },
 
             addPlays: function(playIds) {
-                playIds.forEach( playId => {
+                /* playIds.forEach( playId => {
                     if (this.plays.indexOf(playId) === -1) {
                         this.plays.push(playId);
                     }
-                });
+                });*/
+                playIds.forEach(playId => this.plays.indexOf(playId) < 0 ? this.plays.push(playId) : undefined);
             },
 
             updateDate: function() {
