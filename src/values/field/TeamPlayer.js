4cd977b2e6463f0fb95c49c312e4f72fe024fd8f
Michael <michaels3491@gmail.com>
2015-06-09 15:56:21 -0400

    adding field values directory

---
+++ src/values/field/TeamPlayer.js
@@ -0,0 +1,10 @@
+import Value from '../value.js';
+
+class TeamPlayerValue extends Value {
+    constructor(value) {
+        super();
+
+    }
+}
+
+export default TeamPlayerValue;
---
+++ src/values/field/index.js
@@ -0,0 +1,1 @@
+import TeamPlayerValue from './TeamPlayer.js';
