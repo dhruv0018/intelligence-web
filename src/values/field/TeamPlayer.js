65a008b24e43e9a33091fa4bab14992264befff0
Michael <michaels3491@gmail.com>
2015-06-10 11:54:06 -0400

    working player aspect of team player value

--- src/values/field/TeamPlayer.js
+++ src/values/field/TeamPlayer.js
@@ -1,9 +1,16 @@
 import Value from '../value.js';
 
+/* Fetch angular from the browser scope */
+const angular = window.angular;
+
 class TeamPlayerValue extends Value {
     constructor(value) {
         super();
-
+        let injector = angular.element(document).injector();
+        let players = injector.get('PlayersFactory');
+        let player = players.get(value);
+        this.name = player.firstName + ' ' + player.lastName;
+        this.playerId = player.id;
     }
 }
 
