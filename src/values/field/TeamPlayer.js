32db2ed486757881d66db394404ec73161917b32
Michael <michaels3491@gmail.com>
2015-06-10 16:36:05 -0400

    setting up team player field to properly handle players separately from team

--- src/values/field/TeamPlayer.js
+++ src/values/field/TeamPlayer.js
@@ -4,13 +4,29 @@ import Value from '../value.js';
 const angular = window.angular;
 
 class TeamPlayerValue extends Value {
-    constructor(value) {
+    constructor(value, isPlayer) {
         super();
         let injector = angular.element(document).injector();
-        let players = injector.get('PlayersFactory');
-        let player = players.get(value);
-        this.name = player.firstName + ' ' + player.lastName;
-        this.playerId = player.id;
+
+        //initialization
+        this.name = undefined;
+        this.teamId = undefined;
+        this.playerId = undefined;
+        this.isPlayer = undefined;
+
+        if (!value) return;
+
+        if (isPlayer) {
+            let players = injector.get('PlayersFactory');
+            let player = players.get(value);
+            this.name = player.firstName + ' ' + player.lastName;
+            this.playerId = player.id;
+            this.isPlayer = isPlayer;
+        } else {
+            //todo add team stuff
+            console.log('team stuff');
+        }
+
     }
 }
 
