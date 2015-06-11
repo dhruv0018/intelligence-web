c289efd2cf26903c870b2b51065bf24ce9416909
Michael <michaels3491@gmail.com>
2015-06-11 15:47:00 -0400

    reducing field class to minimal code necessary

--- src/values/field/field.js
+++ src/values/field/field.js
@@ -7,53 +7,15 @@ import Value from '../value.js';
 class Field extends Value {
 
     constructor (field) {
+        if (!field) return;
 
-        switch (arguments.length) {
-
-            case 0:
-
-                throw new Error('Invoking Field.constructor without passing a JSON object');
-        }
-
-        super();
-
+        super(field);
         this.extend(field);
-        //this.value = this.valueFactory();
-        //this.availableValues = []; //todo
-    }
-
-    //valueFactory () {
-    //    let value = {};
-    //    //todo might to be refactored a bit once the entity class is defined
-    //    switch(this.inputType) {
-    //        case 'PLAYER_TEAM_DROPDOWN':
-    //            if (this.type === 'Player') {
-    //                value = new TeamPlayerValue(this.value, true);
-    //            } else {
-    //                value = new TeamPlayerValue(this.value, false);
-    //            }
-    //            break;
-    //        case 'PLAYER_DROPDOWN':
-    //            value = new TeamPlayerValue(this.value, true);
-    //            break;
-    //        case 'TEAM_DROPDOWN':
-    //            value = new TeamPlayerValue(this.value, false);
-    //            break;
-    //        default:
-    //            value = this.value;
-    //            break;
-    //    }
-    //    return value;
-    //}
-
-    toJSON () {
-
-        return '';
     }
 }
 
 /**
  * @module Field
- * @exports entities/field
+ * @exports values/field
  */
 export default Field;
