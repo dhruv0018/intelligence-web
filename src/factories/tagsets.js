1583951a94378f3d8af2e366d57a72342fdad282
Dave Krstin <david@krossover.com>
2015-06-10 20:06:28 -0400

    Created tag entity and refactored Tag Sets Factory to use new tag entity.

--- src/factories/tagsets.js
+++ src/factories/tagsets.js
@@ -1,5 +1,4 @@
-var VARIABLE_PATTERN = /(__\d__)/;
-var VARIABLE_INDEX_PATTERN = /\d/;
+import KrossoverTag from '../entities/tag.js';
 
 var pkg = require('../../package.json');
 
@@ -24,78 +23,13 @@ IntelligenceWebClient.factory('TagsetsFactory', [
 
             extend: function(tagset) {
 
-                var self = this;
+                angular.extend(tagset, this);
 
-                angular.extend(tagset, self);
-
-                var tags = {};
+                let tags = {};
 
                 tagset.tags.forEach(function(tag) {
 
-                    if (angular.isArray(tag.tagVariables)) {
-
-                        var indexedVariables = {};
-
-                        tag.tagVariables.forEach(function(variable, index) {
-
-                            indexedVariables[++index] = variable;
-
-                            if (variable.formations) {
-
-                                let indexedFormations = {};
-
-                                variable.formations.forEach(formation => {
-
-                                    indexedFormations[formation.id] = formation;
-                                });
-
-                                variable.formations = indexedFormations;
-                            }
-                        });
-
-                        tag.tagVariables = indexedVariables;
-                    }
-
-                    ['userScript', 'indexerScript', 'summaryScript'].forEach(function(scriptType) {
-
-                        var script = tag[scriptType];
-
-                        if (script) {
-
-                            /* Split up script into array items and replace variables
-                            * with the actual tag variable object. */
-                            tag[scriptType] = script.split(VARIABLE_PATTERN)
-
-                            /* Filter script items. */
-                            .filter(function(item) {
-
-                                /* Filter out empty items. */
-                                return item.length;
-                            })
-
-                            /* Map script items. */
-                            .map(function(item) {
-
-                                /* If the item is a variable. */
-                                if (VARIABLE_PATTERN.test(item)) {
-
-                                    /* Find the index of the variable in the script. */
-                                    var index = Number(VARIABLE_INDEX_PATTERN.exec(item).pop());
-
-                                    /* Find the tag variable by script index. */
-                                    var tagVariable = tag.tagVariables[index];
-
-                                    /* Store the index position of the tag variable. */
-                                    tagVariable.index = index;
-
-                                    return tagVariable;
-                                }
-
-                                /* If the item is not a variable return it as is. */
-                                else return item;
-                            });
-                        }
-                    });
+                    tag = new KrossoverTag(tag);
 
                     tags[tag.id] = tag;
                     indexedTags[tag.id] = tag;
@@ -108,63 +42,15 @@ IntelligenceWebClient.factory('TagsetsFactory', [
 
             unextend: function(tagset) {
 
-                var self = this;
+                tagset = tagset || this;
 
-                tagset = tagset || self;
+                let copy = angular.copy(tagset);
+                let tags = [];
 
-                var copy = angular.copy(tagset);
+                Object.keys(copy.tags).forEach(tagKey => {
 
-                var tags = [];
-
-                Object.keys(copy.tags).forEach(function(tagKey) {
-
-                    var tag = copy.tags[tagKey];
-
-                    ['userScript', 'indexerScript', 'summaryScript'].forEach(function(scriptType) {
-
-                        var script = tag[scriptType];
-
-                        if (script) {
-
-                            tag[scriptType] = script
-                            .map(function(item) {
-
-                                if (angular.isString(item)) return item;
-                                else return '__' + item.index + '__';
-                            })
-                            .join('');
-                        }
-                    });
-
-                    if (tag.tagVariables) {
-
-                        var tagVariables = [];
-
-                        Object.keys(tag.tagVariables).forEach(function(tagVariableKey) {
-
-                            var tagVariable = tag.tagVariables[tagVariableKey];
-
-                            if (tagVariable.formations) {
-
-                                let formations = [];
-
-                                Object.keys(tagVariable.formations).forEach(tagVariableFormationsKey => {
-
-                                    let formation = tagVariable.formations[tagVariableFormationsKey];
-
-                                    formations.push(formation);
-                                });
-
-                                tagVariable.formations = formations;
-                            }
-
-                            tagVariables[--tagVariableKey] = tagVariable;
-                        });
-
-                        tag.tagVariables = tagVariables;
-                    }
-
-                    tags.push(tag);
+                    let tag = copy.tags[tagKey];
+                    tags.push(JSON.stringify(tag));
                 });
 
                 copy.tags = tags;
