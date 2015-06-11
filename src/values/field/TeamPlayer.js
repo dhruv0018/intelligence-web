8a1d8456feb6200a687ba6eccfd5ba9ee262b114
Michael <michaels3491@gmail.com>
2015-06-11 16:02:43 -0400

    update team player field to handle value calculation

--- src/values/field/TeamPlayer.js
+++ src/values/field/TeamPlayer.js
@@ -1,36 +1,40 @@
-import Field from './field.jd';
+import Field from './field.js';
 
 /* Fetch angular from the browser scope */
 const angular = window.angular;
 
 class TeamPlayerField extends Field {
-    constructor(value, isPlayer = undefined) {
-        super();
-        //let injector = angular.element(document).injector();
-        //
-        ////initialization
-        //this.name = undefined;
-        //this.teamId = undefined;
-        //this.playerId = undefined;
-        //this.isPlayer = isPlayer;
-        //
-        ////todo we might want to have some sort of handling here specially for undef versus null
-        //if (!value) return;
-        //
-        //if (isPlayer) {
-        //    let players = injector.get('PlayersFactory');
-        //    let player = players.get(value);
-        //    this.name = player.firstName + ' ' + player.lastName;
-        //    this.playerId = player.id;
-        //} else {
-        //    //todo add team stuff
-        //    let teams = injector.get('TeamsFactory');
-        //    let team = teams.get(value);
-        //    this.name = team.name;
-        //    this.teamId = team.id;
-        //}
+    constructor(field, isPlayer = false) {
+
+        if (!field) return;
+        super(field);
+
+        let injector = angular.element(document).injector();
+
+        let value = {};
+
+        //initialization
+        value.name = undefined;
+        value.teamId = undefined;
+        value.playerId = undefined;
+        value.isPlayer = isPlayer;
+
+        //todo we might want to have some sort of handling here specially for undef versus null
+        if (isPlayer) {
+            let players = injector.get('PlayersFactory');
+            let player = players.get(field.value);
+            value.name = player.firstName + ' ' + player.lastName;
+            value.playerId = player.id;
+        } else {
+            let teams = injector.get('TeamsFactory');
+            let team = teams.get(field.value);
+            value.name = team.name;
+            value.teamId = team.id;
+        }
+
+        this.value = value;
 
     }
 }
 
-export default TeamPlayerValue;
+export default TeamPlayerField;
