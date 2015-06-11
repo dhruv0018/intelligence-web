f74c22d93a15a50f36fb5d4a43f74c9cf9a93f34
Michael <michaels3491@gmail.com>
2015-06-11 14:48:40 -0400

    extending field in team player

--- src/values/field/TeamPlayer.js
+++ src/values/field/TeamPlayer.js
@@ -1,34 +1,34 @@
-import Value from '../value.js';
+import Field from './field.jd';
 
 /* Fetch angular from the browser scope */
 const angular = window.angular;
 
-class TeamPlayerValue extends Value {
+class TeamPlayerField extends Field {
     constructor(value, isPlayer = undefined) {
         super();
-        let injector = angular.element(document).injector();
-
-        //initialization
-        this.name = undefined;
-        this.teamId = undefined;
-        this.playerId = undefined;
-        this.isPlayer = isPlayer;
-
-        //todo we might want to have some sort of handling here specially for undef versus null
-        if (!value) return;
-
-        if (isPlayer) {
-            let players = injector.get('PlayersFactory');
-            let player = players.get(value);
-            this.name = player.firstName + ' ' + player.lastName;
-            this.playerId = player.id;
-        } else {
-            //todo add team stuff
-            let teams = injector.get('TeamsFactory');
-            let team = teams.get(value);
-            this.name = team.name;
-            this.teamId = team.id;
-        }
+        //let injector = angular.element(document).injector();
+        //
+        ////initialization
+        //this.name = undefined;
+        //this.teamId = undefined;
+        //this.playerId = undefined;
+        //this.isPlayer = isPlayer;
+        //
+        ////todo we might want to have some sort of handling here specially for undef versus null
+        //if (!value) return;
+        //
+        //if (isPlayer) {
+        //    let players = injector.get('PlayersFactory');
+        //    let player = players.get(value);
+        //    this.name = player.firstName + ' ' + player.lastName;
+        //    this.playerId = player.id;
+        //} else {
+        //    //todo add team stuff
+        //    let teams = injector.get('TeamsFactory');
+        //    let team = teams.get(value);
+        //    this.name = team.name;
+        //    this.teamId = team.id;
+        //}
 
     }
 }
