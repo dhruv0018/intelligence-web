d157484a07954b3ade1600943e867e7f87f314f8
Michael <michaels3491@gmail.com>
2015-06-12 13:43:19 -0400

    exporting passing zone

--- src/values/field/index.js
+++ src/values/field/index.js
@@ -1,5 +1,6 @@
 import Field from './Field.js';
 import TeamPlayerField from './TeamPlayer.js';
 import GapField from './Gap.js';
+import PassingZoneField from './PassingZone.js';
 
-export default {Field, TeamPlayerField, GapField};
+export default {Field, TeamPlayerField, GapField, PassingZoneField};
