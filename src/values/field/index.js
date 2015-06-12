35c02012ccc299b6480dc2d52f1cf364489d7f56
Michael <michaels3491@gmail.com>
2015-06-12 14:49:42 -0400

    exporting the data for formations field correctly

--- src/values/field/index.js
+++ src/values/field/index.js
@@ -2,5 +2,6 @@ import Field from './Field.js';
 import TeamPlayerField from './TeamPlayer.js';
 import GapField from './Gap.js';
 import PassingZoneField from './PassingZone.js';
+import FormationField from './Formation.js';
 
-export default {Field, TeamPlayerField, GapField, PassingZoneField};
+export default {Field, TeamPlayerField, GapField, PassingZoneField, FormationField};
