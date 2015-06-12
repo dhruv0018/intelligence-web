82bdb4a3e085e21513c2ccdedcf256f02bc93158
Michael <michaels3491@gmail.com>
2015-06-12 14:35:31 -0400

    formation field

---
+++ src/values/field/Formation.js
@@ -0,0 +1,25 @@
+import Field from './field.js';
+
+/* Fetch angular from the browser scope */
+const angular = window.angular;
+
+class FormationField extends Field {
+    constructor(field) {
+
+        if (!field) return;
+        super(field);
+
+        let injector = angular.element(document).injector();
+
+        let value = {};
+
+        value.formationId = this.value;
+        value.numberPlayers = this.numberPlayers;
+
+        this.value = value;
+
+        //todo note the available values should be in at this point, lmk if they are not
+    }
+}
+
+export default FormationField;
