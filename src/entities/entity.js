ddffd9f92cd2e429fe89596ed663810edf24d78f
Dave Krstin <david@krossover.com>
2015-06-15 13:19:42 -0400

    Changed base entity class to use Object.assign in constructor and removed extend method.

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -3,21 +3,7 @@ class Entity {
 
     constructor (entity) {
 
-        this.extend(entity);
-    }
-
-    extend (entity) {
-
-        let describedEntity = Object.keys(entity).reduce((propertyDescriptor, propertyName) => {
-
-            propertyDescriptor[propertyName] = Object.getOwnPropertyDescriptor(entity, propertyName);
-
-            return propertyDescriptor;
-        }, {});
-
-        Object.defineProperties(this, describedEntity);
-
-        return this;
+        Object.assign(this, entity);
     }
 }
 
