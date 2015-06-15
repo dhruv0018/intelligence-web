768af4a7cf4988c61dfac44cf287ce3ed2d76ee2
Michael <michaels3491@gmail.com>
2015-06-15 16:35:26 -0400

    adding the player field to the export

--- src/values/field/index.js
+++ src/values/field/index.js
@@ -7,10 +7,12 @@ import DropdownField from './Dropdown.js';
 import TextField from './Text.js';
 import YardField from './Yard.js';
 import ArenaField from './Arena.js';
+import PlayerField from './Player.js';
 
 export default
 {
     Field,
+    PlayerField,
     TeamPlayerField,
     GapField,
     PassingZoneField,
