d318eb03f283582648a2e5ab1313abe68eae0b3e
Dave Krstin <david@krossover.com>
2015-06-15 14:17:26 -0400

    Updated tag entity sample data.

--- test/unit/services/sample-data/tag.js
+++ test/unit/services/sample-data/tag.js
@@ -15,239 +15,198 @@ const tag = {
         "type": "FORMATION",
         "isRequired": true,
         "options": [],
-        "formations": {
-            "1": {
-                "id": 1,
-                "name": "Empty",
-                "numberPlayers": 6,
-                "variableId": 17
-            },
-            "2": {
-                "id": 2,
-                "name": "Ace",
-                "numberPlayers": 7,
-                "variableId": 17
-            },
-            "3": {
-                "id": 3,
-                "name": "Right",
-                "numberPlayers": 7,
-                "variableId": 17
-            },
-            "4": {
-                "id": 4,
-                "name": "Left",
-                "numberPlayers": 7,
-                "variableId": 17
-            },
-            "5": {
-                "id": 5,
-                "name": "Splitback",
-                "numberPlayers": 8,
-                "variableId": 17
-            },
-            "6": {
-                "id": 6,
-                "name": "I Right",
-                "numberPlayers": 8,
-                "variableId": 17
-            },
-            "7": {
-                "id": 7,
-                "name": "I Left",
-                "numberPlayers": 8,
-                "variableId": 17
-            },
-            "8": {
-                "id": 8,
-                "name": "Star",
-                "numberPlayers": 9,
-                "variableId": 17
-            },
-            "9": {
-                "id": 9,
-                "name": "Other",
-                "numberPlayers": 0,
-                "variableId": 17
-            }
-        },
-        "index": 1
+        "formations": [{
+            "id": 1,
+            "name": "Empty",
+            "numberPlayers": 6,
+            "variableId": 17
+        }, {
+            "id": 2,
+            "name": "Ace",
+            "numberPlayers": 7,
+            "variableId": 17
+        }, {
+            "id": 3,
+            "name": "Right",
+            "numberPlayers": 7,
+            "variableId": 17
+        }, {
+            "id": 4,
+            "name": "Left",
+            "numberPlayers": 7,
+            "variableId": 17
+        }, {
+            "id": 5,
+            "name": "Splitback",
+            "numberPlayers": 8,
+            "variableId": 17
+        }, {
+            "id": 6,
+            "name": "I Right",
+            "numberPlayers": 8,
+            "variableId": 17
+        }, {
+            "id": 7,
+            "name": "I Left",
+            "numberPlayers": 8,
+            "variableId": 17
+        }, {
+            "id": 8,
+            "name": "Star",
+            "numberPlayers": 9,
+            "variableId": 17
+        }, {
+            "id": 9,
+            "name": "Other",
+            "numberPlayers": 0,
+            "variableId": 17
+        }]
     }, {
         "id": 20,
         "name": "Right Flank",
         "type": "FORMATION",
         "isRequired": true,
         "options": [],
-        "formations": {
-            "35": {
-                "id": 35,
-                "name": "TE Only",
-                "numberPlayers": 1,
-                "variableId": 20
-            },
-            "36": {
-                "id": 36,
-                "name": "TE with Wing",
-                "numberPlayers": 2,
-                "variableId": 20
-            },
-            "37": {
-                "id": 37,
-                "name": "TE with Flanker (Pro)",
-                "numberPlayers": 2,
-                "variableId": 20
-            },
-            "38": {
-                "id": 38,
-                "name": "TE Trips",
-                "numberPlayers": 3,
-                "variableId": 20
-            },
-            "39": {
-                "id": 39,
-                "name": "SE Only",
-                "numberPlayers": 1,
-                "variableId": 20
-            },
-            "40": {
-                "id": 40,
-                "name": "Wing with SE (Slot)",
-                "numberPlayers": 2,
-                "variableId": 20
-            },
-            "41": {
-                "id": 41,
-                "name": "SE and Flanker (Twins)",
-                "numberPlayers": 2,
-                "variableId": 20
-            },
-            "42": {
-                "id": 42,
-                "name": "SE Trips",
-                "numberPlayers": 3,
-                "variableId": 20
-            },
-            "43": {
-                "id": 43,
-                "name": "Bunch Tight",
-                "numberPlayers": 3,
-                "variableId": 20
-            },
-            "44": {
-                "id": 44,
-                "name": "Bunch Wide",
-                "numberPlayers": 3,
-                "variableId": 20
-            },
-            "45": {
-                "id": 45,
-                "name": "Stack",
-                "numberPlayers": 3,
-                "variableId": 20
-            },
-            "46": {
-                "id": 46,
-                "name": "Other",
-                "numberPlayers": 0,
-                "variableId": 20
-            }
-        },
-        "index": 2
+        "formations": [{
+            "id": 35,
+            "name": "TE Only",
+            "numberPlayers": 1,
+            "variableId": 20
+        }, {
+            "id": 36,
+            "name": "TE with Wing",
+            "numberPlayers": 2,
+            "variableId": 20
+        }, {
+            "id": 37,
+            "name": "TE with Flanker (Pro)",
+            "numberPlayers": 2,
+            "variableId": 20
+        }, {
+            "id": 38,
+            "name": "TE Trips",
+            "numberPlayers": 3,
+            "variableId": 20
+        }, {
+            "id": 39,
+            "name": "SE Only",
+            "numberPlayers": 1,
+            "variableId": 20
+        }, {
+            "id": 40,
+            "name": "Wing with SE (Slot)",
+            "numberPlayers": 2,
+            "variableId": 20
+        }, {
+            "id": 41,
+            "name": "SE and Flanker (Twins)",
+            "numberPlayers": 2,
+            "variableId": 20
+        }, {
+            "id": 42,
+            "name": "SE Trips",
+            "numberPlayers": 3,
+            "variableId": 20
+        }, {
+            "id": 43,
+            "name": "Bunch Tight",
+            "numberPlayers": 3,
+            "variableId": 20
+        }, {
+            "id": 44,
+            "name": "Bunch Wide",
+            "numberPlayers": 3,
+            "variableId": 20
+        }, {
+            "id": 45,
+            "name": "Stack",
+            "numberPlayers": 3,
+            "variableId": 20
+        }, {
+            "id": 46,
+            "name": "Other",
+            "numberPlayers": 0,
+            "variableId": 20
+        }]
     }, {
         "id": 21,
         "name": "Left Flank",
         "type": "FORMATION",
         "isRequired": true,
         "options": [],
-        "formations": {
-            "47": {
-                "id": 47,
-                "name": "TE Only",
-                "numberPlayers": 1,
-                "variableId": 21
-            },
-            "48": {
-                "id": 48,
-                "name": "TE with Wing",
-                "numberPlayers": 2,
-                "variableId": 21
-            },
-            "49": {
-                "id": 49,
-                "name": "TE with Flanker (Pro)",
-                "numberPlayers": 2,
-                "variableId": 21
-            },
-            "50": {
-                "id": 50,
-                "name": "TE Trips",
-                "numberPlayers": 3,
-                "variableId": 21
-            },
-            "51": {
-                "id": 51,
-                "name": "SE Only",
-                "numberPlayers": 1,
-                "variableId": 21
-            },
-            "52": {
-                "id": 52,
-                "name": "Wing with SE (Slot)",
-                "numberPlayers": 2,
-                "variableId": 21
-            },
-            "53": {
-                "id": 53,
-                "name": "SE and Flanker (Twins)",
-                "numberPlayers": 2,
-                "variableId": 21
-            },
-            "54": {
-                "id": 54,
-                "name": "SE Trips",
-                "numberPlayers": 3,
-                "variableId": 21
-            },
-            "55": {
-                "id": 55,
-                "name": "Bunch Tight",
-                "numberPlayers": 3,
-                "variableId": 21
-            },
-            "56": {
-                "id": 56,
-                "name": "Bunch Wide",
-                "numberPlayers": 3,
-                "variableId": 21
-            },
-            "57": {
-                "id": 57,
-                "name": "Stack",
-                "numberPlayers": 3,
-                "variableId": 21
-            },
-            "58": {
-                "id": 58,
-                "name": "TE, SE with Wing",
-                "numberPlayers": 2,
-                "variableId": 21
-            },
-            "59": {
-                "id": 59,
-                "name": "Other",
-                "numberPlayers": 0,
-                "variableId": 21
-            }
-        },
-        "index": 3
+        "formations": [{
+            "id": 47,
+            "name": "TE Only",
+            "numberPlayers": 1,
+            "variableId": 21
+        }, {
+            "id": 48,
+            "name": "TE with Wing",
+            "numberPlayers": 2,
+            "variableId": 21
+        }, {
+            "id": 49,
+            "name": "TE with Flanker (Pro)",
+            "numberPlayers": 2,
+            "variableId": 21
+        }, {
+            "id": 50,
+            "name": "TE Trips",
+            "numberPlayers": 3,
+            "variableId": 21
+        }, {
+            "id": 51,
+            "name": "SE Only",
+            "numberPlayers": 1,
+            "variableId": 21
+        }, {
+            "id": 52,
+            "name": "Wing with SE (Slot)",
+            "numberPlayers": 2,
+            "variableId": 21
+        }, {
+            "id": 53,
+            "name": "SE and Flanker (Twins)",
+            "numberPlayers": 2,
+            "variableId": 21
+        }, {
+            "id": 54,
+            "name": "SE Trips",
+            "numberPlayers": 3,
+            "variableId": 21
+        }, {
+            "id": 55,
+            "name": "Bunch Tight",
+            "numberPlayers": 3,
+            "variableId": 21
+        }, {
+            "id": 56,
+            "name": "Bunch Wide",
+            "numberPlayers": 3,
+            "variableId": 21
+        }, {
+            "id": 57,
+            "name": "Stack",
+            "numberPlayers": 3,
+            "variableId": 21
+        }, {
+            "id": 58,
+            "name": "TE, SE with Wing",
+            "numberPlayers": 2,
+            "variableId": 21
+        }, {
+            "id": 59,
+            "name": "Other",
+            "numberPlayers": 0,
+            "variableId": 21
+        }]
     }, {
         "id": 22,
         "name": "Defensive Front",
         "type": "DROPDOWN",
         "isRequired": true,
         "options": ["3", "4", "5", "6", "7", "Unknown"],
-        "formations": {},
-        "index": 4
+        "formations": []
     }],
     "pointsAssigned": null,
     "assignThisTeam": null,
