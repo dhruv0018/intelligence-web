e89b9b8d37329a09b6557475012591bc5c788bc0
Michael <michaels3491@gmail.com>
2015-06-15 11:33:50 -0400

    adding the yard field

---
+++ src/values/field/Yard.js
@@ -0,0 +1,26 @@
+import Field from './Field.js';
+
+/* Fetch angular from the browser scope */
+const angular = window.angular;
+
+class YardField extends Field {
+    constructor(field) {
+        if (!field) return;
+        super(field);
+
+        let injector = angular.element(document).injector();
+        let value = {};
+
+        value.content = this.value;
+
+        this.value = value;
+
+        this.availableOptions = [];
+
+        for (let yard = 1; yard < 100; yard++) {
+            this.availableOptions.push(yard);
+        }
+    }
+}
+
+export default YardField;
