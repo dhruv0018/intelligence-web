ce2faa2a894cd43b9f09c4902ec6a66e1ec97fcf
Alex Young <alexyoung1992@gmail.com>
2015-06-16 01:53:11 -0400

    Improved unit test coverage for Video, TranscodeProfile entities

--- test/unit/services/transcodeProfile.js
+++ test/unit/services/transcodeProfile.js
@@ -1,32 +1,98 @@
+import TranscodeProfile from '../src/entities/transcodeProfile';
+import transcodeProfileData from './sample-data/transcodeProfile';
+
 const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
 
-// TODO: Run Traceur in unit tests
-import TranscodeProfile from '../src/entities/transcodeProfile';
+describe('TranscodeProfile Entity', () => {
 
-describe('TranscodeProfile', () => {
+    const transcodeProfile;
 
     beforeEach(angular.mock.module('intelligence-web-client'));
 
-    it('should exist', inject(transcodeProfile => {
+    beforeEach(() => {
 
-        expect(transcodeProfile).to.exist;
-    }));
+        sampleTranscodeProfile = angular.copy(srcJSON);
+        transcodeProfile = new TranscodeProfile(sampleTranscodeProfile);
+    });
 
-    it('should have a public API', inject(transcodeProfile => {
+    it('should exist', () => {
 
-        expect(transcodeProfile).to.respondTo('id');
-        expect(transcodeProfile).to.respondTo('videoId');
-        expect(transcodeProfile).to.respondTo('targetBitrate');
-        expect(transcodeProfile).to.respondTo('maximumBitrate');
-        expect(transcodeProfile).to.respondTo('minimumBitrate');
-        expect(transcodeProfile).to.respondTo('targetDisplayWidth');
-        expect(transcodeProfile).to.respondTo('targetDisplayHeight');
-        expect(transcodeProfile).to.respondTo('aspectRatio');
+        expect(TranscodeProfile).to.exist;
+    });
+
+    it('should have a public API', () => {
+
+        expect(TranscodeProfile).to.respondTo('id');
+        expect(TranscodeProfile).to.respondTo('videoId');
+        expect(TranscodeProfile).to.respondTo('targetBitrate');
+        expect(TranscodeProfile).to.respondTo('maximumBitrate');
+        expect(TranscodeProfile).to.respondTo('minimumBitrate');
+        expect(TranscodeProfile).to.respondTo('targetDisplayWidth');
+        expect(TranscodeProfile).to.respondTo('targetDisplayHeight');
+        expect(TranscodeProfile).to.respondTo('aspectRatio');
         expect(TranscodeProfile).to.respondTo('title');
-        expect(transcodeProfile).to.respondTo('status');
-        expect(transcodeProfile).to.respondTo('url');
-        expect(transcodeProfile).to.respondTo('resourceUrl');
-    }));
+        expect(TranscodeProfile).to.respondTo('status');
+        expect(TranscodeProfile).to.respondTo('url');
+        expect(TranscodeProfile).to.respondTo('resourceUrl');
+    });
+
+    it('should throw an Error if constructor is called without parameters.', () => {
+
+        expect(() => new TranscodeProfile()).to.throw(Error);
+    });
+
+    it('should have called super in the constructor', () => {
+
+        transcodeProfile.super = sinon.spy();
+
+        let instantiation = new TranscodeProfile(sampleTranscodeProfile);
+
+        assert(transcodeProfile.super.should.have.been.called);
+    });
+
+    it('should have certain properties when instantiated', () => {
+
+        expect(transcodeProfile).to.contain.keys([
+            'id',
+            'videoId',
+            'url',
+            'videoUrl',
+            'profile',
+            'transcodeProfileId'
+            'targetBitrate',
+            'maximumBitrate',
+            'minimumBitrate',
+            'description',
+            'targetDisplayWidth',
+            'targetDisplayHeight',
+            'aspectRatio',
+            'title',
+            'resourceUrl'
+        ]);
+    });
+
+    it('should have called toJSON on a JSON.stringify call', () => {
+
+        transcodeProfile.toJSON = sinon.spy();
+
+        JSON.stringify(transcodeProfile);
+
+        assert(transcodeProfile.toJSON.should.have.been.called);
+    });
+
+    it('should restore the original JSON on JSON.stringify calls', () => {
+
+        transcodeProfile = transcodeProfile.toJSON();
+
+        expect(transcodeProfile.id).to.equal(srcJSON.id);
+        expect(transcodeProfile.videoId).to.equal(srcJSON.videoId);
+
+        expect(transcodeProfile.transcodeProfile).to.be.an('object');
+        expect(transcodeProfile.transcodeProfile).to.deep.equal(srcJSON.transcodeProfile);
+
+        expect(transcodeProfile.status).to.equal(srcJSON.status);
+        expect(transcodeProfile.videoUrl).to.equal(srcJSON.videoUrl);
+    });
 });
--- test/unit/services/video.js
+++ test/unit/services/video.js
@@ -1,27 +1,89 @@
+import Video from '../../../src/entities/video';
+import videoData from './sample-data/video';
+
 const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
 
-// TODO: Run Traceur in unit tests
-import Video from '../src/entities/video';
+const srcJSON = videoData;
 
-describe('Video', () => {
+describe('Video Entity', () => {
+
+    const video;
 
     beforeEach(angular.mock.module('intelligence-web-client'));
 
-    it('should exist', inject(video => {
+    beforeEach(() => {
 
-        expect(video).to.exist;
-    }));
+        sampleVideo = angular.copy(srcJSON);
+        video = new Video(sampleVideo);
+    });
 
-    it('should have a public API', inject(video => {
+    it('should exist', () => {
 
-        expect(video).to.respondTo('id');
-        expect(video).to.respondTo('guid');
-        expect(video).to.respondTo('status');
-        expect(video).to.respondTo('transcodeProfiles');
-        expect(video).to.respondTo('duration');
-        expect(video).to.respondTo('thumbnail');
-        expect(video).to.respondTo('resourceUrls');
-    }));
+        expect(Video).to.exist;
+    });
+
+    it('should have a public API', () => {
+
+        expect(Video).to.respondTo('id');
+        expect(Video).to.respondTo('guid');
+        expect(Video).to.respondTo('status');
+        expect(Video).to.respondTo('transcodeProfiles');
+        expect(Video).to.respondTo('duration');
+        expect(Video).to.respondTo('thumbnail');
+        expect(Video).to.respondTo('resourceUrls');
+    });
+
+    it('should throw an Error if constructor is called without parameters.', () => {
+
+        expect(() => new Video()).to.throw(Error);
+    });
+
+    it('should have called super in the constructor', () => {
+
+        video.super = sinon.spy();
+
+        let instantiation = new Video(sampleVideo);
+
+        assert(video.super.should.have.been.called);
+    });
+
+    it('should have certain properties when instantiated', () => {
+
+        expect(video).to.contain.keys([
+            'id',
+            'guid',
+            'status',
+            'transcodeProfiles',
+            'videoTranscodeProfiles',
+            'duration',
+            'thumnbail',
+            'resourceUrls'
+        ]);
+    });
+
+     it('should have called toJSON on a JSON.stringify call', () => {
+
+         video.toJSON = sinon.spy();
+
+         JSON.stringify(video);
+
+         assert(video.toJSON.should.have.been.called);
+     });
+
+    it('should restore the original JSON on JSON.stringify calls', () => {
+
+        video = video.toJSON();
+
+        expect(video.id).to.equal(srcJSON.id);
+        expect(video.guid).to.equal(srcJSON.guid);
+        expect(video.status).to.equal(srcJSON.status);
+
+        expect(video.videoTranscodeProfiles).to.be.an('array');
+        expect(video.videoTranscodeProfiles).to.deep.equal(srcJSON.videoTranscodeProfiles);
+
+        expect(video.duration).to.equal(srcJSON.duration);
+        expect(video.thumbnail).to.equal(srcJSON.thumbnail);
+    });
 });
