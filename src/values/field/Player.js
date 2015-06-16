abaab77ce62ebba87ef733ce112c35b7470e0b5f
Michael <michaels3491@gmail.com>
2015-06-16 11:43:52 -0400

    toJSON for player

--- src/values/field/Player.js
+++ src/values/field/Player.js
@@ -12,16 +12,28 @@ class PlayerField extends Field {
         let injector = angular.element(document).injector();
         let players = injector.get('PlayersFactory');
 
-        let value = {};
+        //initialization
+        let value = {
+            name: null,
+            playerId: null
+        };
 
         if (field.value) {
             let player = players.get(field.value);
             value.name = player.firstName + ' ' + player.lastName;
             value.playerId = player.id;
-
-            this.value = value;
         }
+        this.value = value;
+    }
 
+    toJSON(){
+        let variableValue = {};
+        //todo make a constant for type
+        variableValue = {
+            type: 'Player',
+            value: this.value.playerId
+        };
+        return JSON.stringify(variableValue);
     }
 }
 
