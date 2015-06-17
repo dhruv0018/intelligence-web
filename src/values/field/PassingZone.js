6e53042986db8ec4376db17e60f8ebc0befff75d
Michael <michaels3491@gmail.com>
2015-06-17 17:46:22 -0400

    passing zone setter and getter

--- src/values/field/PassingZone.js
+++ src/values/field/PassingZone.js
@@ -13,20 +13,29 @@ class PassingZoneField extends Field {
         this.ZONES = injector.get('ZONES');
         this.ZONE_IDS = injector.get('ZONE_IDS');
 
-        let value = {
-            zoneId: !field.isRequired ? 'Optional' : undefined
+        let zone = {
+            name: !field.isRequired ? 'Optional' : undefined,
+            zoneId: !field.isRequired ? null : undefined,
+            keyboardShortcut: null
         };
 
-        if (field.value) {
-            let zone = this.ZONES[this.ZONE_IDS[field.value]];
-            value.zoneId = zone.value;
-            value.keyboardShortcut = zone.shortcut;
-            value.name = zone.name;
-        }
+        if (field.value) zone = this.ZONES[this.ZONE_IDS[field.value]];
 
-        this.value = value;
+        this.currentValue = zone;
 
         this.availableValues = Object.keys(this.ZONES).map(key => this.ZONES[key]);
+    }
+
+    get currentValue() {
+        return this.value;
+    }
+
+    set currentValue(zone) {
+        let value = {};
+        value.zoneId = zone.value;
+        value.keyboardShortcut = zone.shortcut;
+        value.name = zone.name;
+        this.value = value;
     }
 
     toJSON(){
