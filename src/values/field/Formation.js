e10503704f8a3f9cd95db7b951134ea6584ce18e
Michael <michaels3491@gmail.com>
2015-06-18 19:02:27 -0400

    getter and setter formation

--- src/values/field/Formation.js
+++ src/values/field/Formation.js
@@ -11,20 +11,27 @@ class FormationField extends Field {
 
         let injector = angular.element(document).injector();
 
-        let value = {
+        let formation = {
             formationId: !field.isRequired ? null : undefined,
             numberOfPlayers: 0
         };
 
-        if (field.value) {
-            let formation = this.formations[field.value];
-            value.formationId = formation.id;
-            value.numberOfPlayers = formation.numberPlayers;
-            value.name = formation.name;
-        }
+        if (field.value) formation = this.formations[field.value];
 
-        this.value = value;
+        this.currentValue = formation;
         this.availableValues = Object.keys(this.formations).map(key => this.formations[key]);
+    }
+
+    get currentValue() {
+        return this.value;
+    }
+
+    set currentValue(formation) {
+        let value = {};
+        value.formationId = formation.id;
+        value.numberOfPlayers = formation.numberPlayers;
+        value.name = formation.name;
+        this.value = value;
     }
 
     toJSON() {
