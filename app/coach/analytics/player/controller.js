bc6e2d0b6ff29f754699b169baad38c3eafc7a45
Alex Young <alexyoung1992@gmail.com>
2015-06-19 00:16:58 -0400

    Removed excess templating and controller logic in player stats

--- app/coach/analytics/player/controller.js
+++ app/coach/analytics/player/controller.js
@@ -1,68 +1,32 @@
 /* Fetch angular from the browser scope */
-var angular = window.angular;
-var PlayerAnalytics = angular.module('Coach.Analytics.Player');
+const angular = window.angular;
+const PlayerAnalytics = angular.module('Coach.Analytics.Player');
 
 PlayerAnalytics.controller('PlayerAnalyticsController', PlayerAnalyticsController);
 
 PlayerAnalyticsController.$inject = [
     '$scope',
-    '$state',
-    '$stateParams',
     'SessionService',
-    'LeaguesFactory',
     'TeamsFactory',
-    'PlayersFactory',
-    'GAME_TYPES'
+    'PlayersFactory'
 ];
 
 /**
  * Player Analytics page controller
  */
-
 function PlayerAnalyticsController(
     $scope,
-    $state,
-    $stateParams,
     session,
-    leagues,
     teams,
-    players,
-    GAME_TYPES
+    players
 ) {
-    var teamId = session.currentUser.currentRole.teamId;
-    var team = teams.get(teamId);
-    var league = leagues.get(team.leagueId);
+
+    const teamId = session.getCurrentTeamId();
+    const team = teams.get(teamId);
 
     players.load({rosterId: team.roster.id}).then(function(data) {
         $scope.options = data;
     });
-
-    $scope.seasons = league.seasons;
-    $scope.loadingTables = true;
-    $scope.statsData = {};
-
-    //Game type constants
-    $scope.CONFERENCE = GAME_TYPES.CONFERENCE;
-    $scope.NON_CONFERENCE = GAME_TYPES.NON_CONFERENCE;
-    $scope.PLAYOFF = GAME_TYPES.PLAYOFF;
-
-    $scope.filterQuery = {
-        seasonId: league.seasons[0].id,
-        gameType: ''
-    };
-
-    team.generateStats($scope.filterQuery).then(function(statsData) {
-        $scope.statsData = statsData;
-        $scope.loadingTables = false;
-    });
-
-    $scope.generateStats = function() {
-        $scope.loadingTables = true;
-        team.generateStats($scope.filterQuery).then(function(statsData) {
-            $scope.statsData = statsData;
-            $scope.loadingTables = false;
-        });
-    };
 }
 
 require('./controller');
