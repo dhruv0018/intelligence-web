326220b1864796828a98ae22ca042695669a4b54
Michael <michaels3491@gmail.com>
2015-06-18 20:31:50 -0400

    arena getter setter

--- src/values/field/Arena.js
+++ src/values/field/Arena.js
@@ -8,20 +8,36 @@ class ArenaField extends Field {
         super(field);
 
         let injector = angular.element(document).injector();
+        this.regionMap = injector.get('ARENA_REGIONS_BY_ID');
+
         //todo look into initialization of arena value
-        let value = {
+        let arena = {
             regionId: !field.isRequired ? null : undefined,
-            coordinates: !field.isRequired ? {} : {}
+            coordinates: !field.isRequired ? {} : {},
+            name: !field.isRequired ? 'Optional' : undefined
         };
 
         if (field.value && field.value.region && field.value.region.id) {
-            value.coordinates = this.value.coordinates;
-            value.regionId = this.value.region.id;
+            arena.coordinates = this.value.coordinates;
+            arena.regionId = this.value.region.id;
+            arena.name = this.regionMap[arena.regionId].name;
         }
 
-        this.value = value;
+        this.currentValue = arena;
 
         this.availableValues = null;
+    }
+
+    get currentValue() {
+        return this.value;
+    }
+
+    set currentValue(arena) {
+        let value = {};
+        value.coordinates = arena.coordinates;
+        value.regionId = arena.region.id;
+        value.name = this.regionMap[arena.region.id].name;
+        this.value = value;
     }
 
     toJSON() {
