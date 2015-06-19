9b3737444ff04c215045cace9105a6f46c8fd00e
Michael <michaels3491@gmail.com>
2015-06-19 10:42:26 -0400

    getter and setter for the team

--- src/values/field/Team.js
+++ src/values/field/Team.js
@@ -10,22 +10,39 @@ class TeamField extends Field {
         super(field);
 
         let injector = angular.element(document).injector();
+        this.teams = injector.get('TeamsFactory');
 
         //initialization
-        let value = {
+        let teamOption = {
             name: !field.isRequired ? 'Optional' : undefined,
             teamId: (!field.isRequired && field.type === 'Team') ? null : undefined
         };
 
         if (field.value) {
-            let teams = injector.get('TeamsFactory');
-            let team = teams.get(field.value);
-            value.name = team.name;
-            value.teamId = team.id;
+            let team = this.teams.get(field.value);
+            teamOption.name = team.name;
+            teamOption.teamId = team.id;
         }
 
-        this.value = value;
+        this.currentValue = teamOption;
+        this.availableOptions = []; //todo blocker
+    }
 
+    get currentValue() {
+        return this.value;
+    }
+
+    set currentValue(teamOption) {
+        let value = {};
+        if (!teamOption.id) {
+            value = teamOption;
+            this.value = value;
+            return;
+        }
+        let team = this.teams.get(teamOption.id);
+        teamOption.name = team.name;
+        teamOption.teamId = team.id;
+        this.value = value;
     }
 
     toJSON() {
