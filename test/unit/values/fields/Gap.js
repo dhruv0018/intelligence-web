770705899bba9d7a3023ce2d20287c7967e669cd
Michael <michaels3491@gmail.com>
2015-06-23 17:42:36 -0400

    numerous gap event field unit tests

--- test/unit/values/fields/Gap.js
+++ test/unit/values/fields/Gap.js
@@ -9,19 +9,20 @@ const should  = chai.should();
 const tagVariable = GapFieldData.Tag;
 const eventVariable = GapFieldData.Event;
 
-describe('Gap Tag Field', () => {
-    beforeEach(angular.mock.module('intelligence-web-client'));
-
+describe('General Gap Field', () => {
+    let tagField = new GapField(tagVariable);
     it('The Gap Class should Exist', () => {
         expect(GapField).to.exist;
     });
 
     it('Should have correct input type', () => {
-        let tagField = new GapField(tagVariable);
         expect(tagField.inputType).to.exist;
         expect(tagField.inputType).to.equal('GAP');
     });
 
+});
+
+describe('Gap Tag Field', () => {
     it('Should be initialized correctly if required', () => {
         let localTagVariable = angular.copy(tagVariable);
         localTagVariable.isRequired = true;
@@ -42,5 +43,54 @@ describe('Gap Tag Field', () => {
         expect(value.gapId).to.be.null;
         expect(value.name).to.equal('Optional');
         expect(value.keyboardShortcut).to.be.undefined;
+    });
+
+});
+
+describe('Gap Event Field', () => {
+
+    it('Should have properly set value if required', () => {
+        let localEventVariable = angular.copy(eventVariable);
+        localEventVariable.isRequired = true;
+        let eventField = new GapField(localEventVariable);
+        let value = eventField.currentValue;
+
+        expect(value.gapId).to.equal(1);
+        expect(value.name).to.equal('D Left');
+        expect(value.keyboardShortcut).to.equal('DL');
+    });
+
+    it('Should set values properly', () => {
+        let localEventVariable = angular.copy(eventVariable);
+        localEventVariable.isRequired = true;
+        let eventField = new GapField(localEventVariable);
+        eventField.currentValue = eventField.availableValues[1];
+        let value = eventField.currentValue;
+
+        expect(value.gapId).to.equal(2);
+        expect(value.name).to.equal('C Left');
+        expect(value.keyboardShortcut).to.equal('CL');
+    });
+
+    it('toJSON should serialize to the right format if the field has a value', () => {
+        let localEventVariable = angular.copy(eventVariable);
+        localEventVariable.isRequired = true;
+        let eventField = new GapField(localEventVariable);
+        let value = eventField.currentValue;
+        let serializedField = eventField.toJSON();
+        expect(serializedField).to.equal('{"type":null,"value":"1"}');
+    });
+
+    it('toJSON should serialize to the right format if the field has no value', () => {
+        let localEventVariable = angular.copy(eventVariable);
+
+        localEventVariable.isRequired = false;
+        localEventVariable.value = undefined;
+
+        let eventField = new GapField(localEventVariable);
+        let value = eventField.currentValue;
+        let serializedField = eventField.toJSON();
+
+        expect(serializedField).to.equal('{"type":null,"value": null}');
     });
 });
