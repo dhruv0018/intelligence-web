dc3057ef130629ed2a98671cd3fd1f3eb2d2e5b3
Michael <michaels3491@gmail.com>
2015-06-24 18:12:49 -0400

    unit testing text, first two suites

---
+++ test/unit/values/fields/Text.js
@@ -0,0 +1,102 @@
+// jshint ignore: start
+import TextField from '../../../../src/values/field/Text.js';
+import TextFieldData from '../sample-data/Text.js';
+
+const assert  = chai.assert;
+const expect  = chai.expect;
+const should  = chai.should();
+
+const tagVariable = TextFieldData.Tag;
+const eventVariable = TextFieldData.Event;
+
+describe('General Text Field', () => {
+    let tagField = new TextField(tagVariable);
+    it('The Text Class should Exist', () => {
+        expect(TextField).to.exist;
+    });
+
+    it('Should have correct input type', () => {
+        expect(tagField.inputType).to.exist;
+        expect(tagField.inputType).to.equal('TEXT');
+    });
+
+});
+
+describe('Text Tag Field', () => {
+    it('Should be initialized correctly if required', () => {
+        let localTagVariable = angular.copy(tagVariable);
+        localTagVariable.isRequired = true;
+        let tagField = new TextField(localTagVariable);
+        let value = tagField.currentValue;
+        expect(value.content).to.be.undefined;
+        expect(value.name).to.equal('Select');
+    });
+
+    it('Should be initialized correctly if not required', () => {
+        let localTagVariable = angular.copy(tagVariable);
+        localTagVariable.isRequired = false;
+        let tagField = new TextField(localTagVariable);
+        let value = tagField.currentValue;
+        expect(value.content).to.be.null;
+        expect(value.name).to.equal('Optional');
+    });
+
+});
+//
+// describe('Text Event Field', () => {
+//
+//     it('Should have properly set value if required', () => {
+//         let localEventVariable = angular.copy(eventVariable);
+//         localEventVariable.isRequired = true;
+//         let eventField = new TextField(localEventVariable);
+//         let value = eventField.currentValue;
+//
+//         expect(value.zoneId).to.equal(1);
+//         expect(value.name).to.equal('Loss Far Left');
+//         expect(value.keyboardShortcut).to.equal('FL');
+//     });
+//
+//     it('Should set values properly', () => {
+//         let localEventVariable = angular.copy(eventVariable);
+//         localEventVariable.isRequired = true;
+//         let eventField = new TextField(localEventVariable);
+//         eventField.currentValue = eventField.availableValues[2];
+//         let value = eventField.currentValue;
+//
+//         expect(value.gapId).to.equal(2);
+//         expect(value.name).to.equal('Loss Left');
+//         expect(value.keyboardShortcut).to.equal('LL');
+//     });
+//
+//     it('toJSON should serialize to the right format if the field has a value', () => {
+//         let localEventVariable = angular.copy(eventVariable);
+//         localEventVariable.isRequired = true;
+//         let eventField = new TextField(localEventVariable);
+//         let value = eventField.currentValue;
+//         let serializedField = eventField.toJSON();
+//         expect(serializedField).to.equal('{"type":null,"value":"1"}');
+//     });
+//
+//     it('toJSON should serialize to the right format if the field has no value', () => {
+//         let localEventVariable = angular.copy(eventVariable);
+//         localEventVariable.isRequired = false;
+//         localEventVariable.value = undefined;
+//
+//         let eventField = new TextField(localEventVariable);
+//         let value = eventField.currentValue;
+//         let serializedField = eventField.toJSON();
+//
+//         expect(serializedField).to.equal('{"type":null,"value":null}');
+//     });
+//
+//     it('Should be able to switch back to an optional value from a set value', () => {
+//         let localEventVariable = angular.copy(eventVariable);
+//         localEventVariable.isRequired = false;
+//         let eventField = new TextField(localEventVariable);
+//         let value = eventField.currentValue;
+//         expect(value.zoneId).to.not.be.null;
+//         eventField.currentValue = eventField.availableValues[0];
+//         value = eventField.currentValue;
+//         expect(value.zoneId).to.be.null;
+//     });
+// });
