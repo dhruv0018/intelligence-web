825cecab57d9808417e135da2e24e76baab05cdb
Dave Krstin <david@krossover.com>
2015-06-25 19:03:23 -0400

    Reverted plays factory to pre-refactor state (with new extend/unextend) to ease master merges.

--- src/factories/plays.js
+++ src/factories/plays.js
@@ -1,174 +1,174 @@
 import KrossoverPlay from '../entities/play.js';
 import KrossoverPlayDataDependencies from '../entities/playDataDependencies.js';
 
-const pkg = require('../../package.json');
+var pkg = require('../../package.json');
 
 /* Fetch angular from the browser scope */
-const angular = window.angular;
+var angular = window.angular;
 
-const IntelligenceWebClient = angular.module(pkg.name);
+var IntelligenceWebClient = angular.module(pkg.name);
 
-IntelligenceWebClient.factory('PlaysFactory', PlaysFactory);
 IntelligenceWebClient.factory('KrossoverPlayDataDependencies', KrossoverPlayDataDependencies);
 
-PlaysFactory.$inject = [
-    'KrossoverPlayDataDependencies',
-    '$injector',
-    'config',
-    '$sce',
-    'VIDEO_STATUSES',
-    'PlaysResource',
-    'BaseFactory',
-    'TagsetsFactory',
-    'Utilities'
-];
+IntelligenceWebClient.factory('PlaysFactory', [
+    'KrossoverPlayDataDependencies', '$injector', 'config', '$sce', 'VIDEO_STATUSES', 'PlaysResource', 'BaseFactory', 'TagsetsFactory', 'Utilities', 'CUEPOINT_CONSTANTS',
+    function(KrossoverPlay, $injector, config, $sce, VIDEO_STATUSES, PlaysResource, BaseFactory, tagsets, utils, CUEPOINT_CONSTANTS) {
 
-function PlaysFactory (
-    KrossoverPlay,
-    $injector,
-    config,
-    $sce,
-    VIDEO_STATUSES,
-    PlaysResource,
-    BaseFactory,
-    tagsets,
-    utils
-) {
+        var PlaysFactory = {
 
-    let factory = {
+            PAGE_SIZE: 1000,
 
-        PAGE_SIZE: 1000,
+            description: 'plays',
 
-        description: 'plays',
+            model: 'PlaysResource',
 
-        model: 'PlaysResource',
+            storage: 'PlaysStorage',
 
-        storage: 'PlaysStorage',
+            extend: function (play) {
 
-        extend: function (play) {
+                angular.extend(play, this);
+                play = new KrossoverPlay(play);
 
-            angular.extend(play, this);
-            play = new KrossoverPlay(play);
+                return play;
+            },
 
-            return play;
-        },
+            unextend: function (play) {
 
-        unextend: function (play) {
+                play = play || this;
 
-            play = play || this;
+                return play.toJSON();
+            },
 
-            return play.toJSON();
-        },
+            filterPlays: function(filterId, resources, success, error) {
+                var self = this;
+                var playIds = [];
 
-        filterPlays: function(filterId, resources, success, error) {
+                angular.forEach(resources.plays, function(play) {
+                    playIds.push(play.id);
+                });
 
-            let self = this;
-            let playIds = [];
+                var filter = {
+                    plays: {},
+                    options: {
+                        teamId: resources.teamId,
+                        playerId: resources.playerId,
+                        customTagId: resources.customTagId
+                    }
+                };
 
-            angular.forEach(resources.plays, (play) => playIds.push(play.id));
+                filter.plays[resources.game.id] = playIds;
 
-            let filter = {
+                var newPlayList = new PlaysResource(filter);
 
-                plays: {},
-                options: {
+                var callback = success || function(plays) {
+                    return plays;
+                };
 
-                    teamId: resources.teamId,
-                    playerId: resources.playerId,
-                    customTagId: resources.customTagId
+                error = error || function() {
+                    throw new Error('could not filter plays');
+                };
+
+                return newPlayList.$filter({filterId: filterId.filterId}, callback, error);
+            },
+
+            load (filter) {
+
+                return tagsets.load().then(() => { return this.baseLoad(filter); });
+            },
+
+            /**
+             * Gets the video sources for a play.
+             * If a play has a clip the clips video transcode profiles are
+             * mapped to video sources that can be used in videogular.
+             * @returns Array - an array of video sources.
+             */
+            getVideoSources: function() {
+
+                var self = this;
+
+                /* If there is no clip for the play, return an empty array. */
+                if (!self.clip) return [];
+
+                /* Get the video transcode profiles. */
+                var profiles = self.clip.videoTranscodeProfiles;
+
+                /* Map the video transcode profiles to video sources. */
+                return profiles.map(profileToSource);
+
+                function profileToSource(profile) {
+
+                    /* If the transcode profile is complete. */
+                    if (profile.status === VIDEO_STATUSES.COMPLETE.id) {
+
+                        /* Create a video source. */
+                        var source = {
+                            type: 'video/mp4',
+                            src: $sce.trustAsResourceUrl(profile.videoUrl)
+                        };
+
+                        return source;
+                    }
                 }
-            };
+            },
 
-            filter.plays[resources.game.id] = playIds;
+            // TODO: move to list modelling
+            batchSave: function(plays) {
+                let model = $injector.get(this.model);
+                let parameters = {};
 
-            let newPlayList = new PlaysResource(filter);
+                plays = plays.map(play => {
+                    return this.unextend(play);
+                });
 
-            let callback = success || function (plays) {
+                let batchUpdate = model.batchUpdate(parameters, plays);
+
+                return  batchUpdate.$promise;
+            },
+
+            filterByCustomTags: function(plays, customTagIds) {
+                if (!customTagIds || !customTagIds.length) return plays;
+
+                plays = plays.filter(play => {
+                    return customTagIds.every(tagId => {
+                        return !!~play.customTagIds.indexOf(tagId);
+                    });
+                });
 
                 return plays;
-            };
+            },
 
-            error = error || function () {
+            /** Gets the events time relative to a play
+            * @param {event} event
+            * @param [{event}] play The play it's relative to or the thisObject
+            */
+            getEventsRelativeTime: function (event, play = this) {
 
-                throw new Error('could not filter plays');
-            };
+                if (angular.isUndefined(play.startTime)) throw new Error('Play parameter is missing startTime');
+                if (!event) throw new Error('Missing "event" parameter');
+                if (angular.isUndefined(event.time)) throw new Error('event.time is undefined');
 
-            return newPlayList.$filter({filterId: filterId.filterId}, callback, error);
-        },
+                return event.time - play.startTime;
+            },
 
-        load (filter) {
+            getEventCuePoints: function() {
 
-            return tagsets.load().then(() => this.baseLoad(filter));
-        },
+                let cuePoints = [];
 
-        /**
-         * Gets the video sources for a play.
-         * If a play has a clip the clips video transcode profiles are
-         * mapped to video sources that can be used in videogular.
-         * @returns Array - an array of video sources.
-         */
-        getVideoSources: function () {
+                if (!this.events) return cuePoints;
 
-            let self = this;
-
-            /* If there is no clip for the play, return an empty array. */
-            if (!self.clip) return [];
-
-            /* Get the video transcode profiles. */
-            let profiles = self.clip.videoTranscodeProfiles;
-
-            /* Map the video transcode profiles to video sources. */
-            return profiles.map(profileToSource);
-
-            function profileToSource (profile) {
-
-                /* If the transcode profile is complete. */
-                if (profile.status === VIDEO_STATUSES.COMPLETE.id) {
-
-                    /* Create a video source. */
-                    let source = {
-
-                        type: 'video/mp4',
-                        src: $sce.trustAsResourceUrl(profile.videoUrl)
+                cuePoints = this.events.map(function(event) {
+                    return {
+                        time: event.time,
+                        type: CUEPOINT_CONSTANTS.TYPES.EVENT
                     };
-
-                    return source;
-                }
-            }
-        },
-
-        // TODO: move to list modelling
-        batchSave: function (plays) {
-
-            let model = $injector.get(this.model);
-            let parameters = {};
-
-            plays = plays.map(play => {
-
-                return this.unextend(play);
-            });
-
-            let batchUpdate = model.batchUpdate(parameters, plays);
-
-            return  batchUpdate.$promise;
-        },
-
-        filterByCustomTags: function (plays, customTagIds) {
-
-            if (!customTagIds || !customTagIds.length) return plays;
-
-            plays = plays.filter(play => {
-
-                return customTagIds.every(tagId => {
-
-                    return !!~play.customTagIds.indexOf(tagId);
                 });
-            });
 
-            return plays;
-        }
-    };
+                return cuePoints;
+            }
+        };
 
-    angular.augment(factory, BaseFactory);
+        angular.augment(PlaysFactory, BaseFactory);
 
-    return factory;
-}
+        return PlaysFactory;
+    }
+]);
