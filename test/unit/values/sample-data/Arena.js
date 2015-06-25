f2ea0d279195513f5ab5db3dc5f94715fee95887
Michael <michaels3491@gmail.com>
2015-06-25 17:41:58 -0400

    scaffolding for arena unit tests

---
+++ test/unit/values/sample-data/Arena.js
@@ -0,0 +1,33 @@
+let ArenaFieldData = {};
+
+ArenaFieldData.Tag = {
+    formations: [],
+    id: 1,
+    index: 1,
+    inputType: 'ARENA',
+    isRequired: true,
+    options: [],
+    type: null
+};
+
+ArenaFieldData.Event = {
+    formations: [],
+    id: 1,
+    index: 1,
+    inputType: 'ARENA',
+    isRequired: true,
+    options: [],
+    type: null,
+    value: {
+        coordinates: {
+            x: 10,
+            y: 10
+        },
+        region: {
+            id: 1
+        }
+    }
+};
+
+
+export default ArenaFieldData;
