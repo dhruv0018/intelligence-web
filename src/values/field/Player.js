72787da5688367c5bdd806fcf3268464fc57729d
Michael <michaels3491@gmail.com>
2015-06-25 11:29:44 -0400

    working getter for player field name

--- src/values/field/Player.js
+++ src/values/field/Player.js
@@ -4,30 +4,42 @@ import Field from './Field.js';
 const angular = window.angular;
 
 class PlayerField extends Field {
-    constructor(players, field) {
+    //constructor(players, field) {
+    constructor(field) {
 
         if (!field) return;
         super(field);
 
-        let injector = angular.element(document).injector();
-        this.players = players;
-        //injector.get('PlayersFactory');
-
         //initialization
         let playerOption = {
-            name: !field.isRequired ? 'Optional' : undefined,
             playerId: (!field.isRequired && field.type === 'Player') ? null : undefined
         };
-
-        if (field.value) {
-            let player = this.players.get(field.value);
-            playerOption.name = player.firstName + ' ' + player.lastName;
-            playerOption.playerId = player.id;
-        }
-
+        if (field.value) playerOption.playerId = field.value;
         this.currentValue = playerOption;
 
         this.availableValues = []; //todo blocker
+    }
+
+    get name() {
+        let calculatedName = !this.isRequired ? 'Optional' : 'Select';
+        let value = this.currentValue;
+        let playerId = value.playerId;
+        if (playerId) {
+            let injector = angular.element(document).injector();
+            let players = injector.get('PlayersFactory');
+            let player = players.get(playerId);
+            calculatedName = player.firstName + ' ' + player.lastName;
+        }
+        return calculatedName;
+    }
+
+    set name (calculatedName) {
+        // let calculatedName = !this.isRequired ? 'Optional' : 'Select';
+        // let injector = angular.element(document).injector();
+        // let players = injector.get('PlayersFactory');
+        // let player = players.get(playerId);
+        // calculatedName = player.firstName + ' ' + player.lastName;
+        //this.value.name = calculatedName;
     }
 
     get currentValue() {
@@ -35,15 +47,9 @@ class PlayerField extends Field {
     }
 
     set currentValue(playerOption) {
-        let value = {};
-        if (!playerOption.id) {
-            value = playerOption;
-            this.value = value;
-            return;
-        }
-        let player = this.players.get(playerOption.playerId);
-        value.name = player.firstName + ' ' + player.lastName;
-        value.playerId = player.id;
+        let value = {
+            playerId: playerOption.playerId
+        };
         this.value = value;
     }
 
@@ -58,7 +64,7 @@ class PlayerField extends Field {
     }
 }
 
-PlayerField.$inject = ['PlayersFactory'];
-var IntelligenceWebClient = angular.module('intelligence-web-client');
-IntelligenceWebClient.factory('PlayersField', () => PlayersField);
+//PlayerField.$inject = ['PlayersFactory'];
+//var IntelligenceWebClient = angular.module('intelligence-web-client');
+//IntelligenceWebClient.factory('PlayersField', () => PlayersField);
 export default PlayerField;
