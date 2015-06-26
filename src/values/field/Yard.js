e1639dde3051f9e933633a877f89507603e9e032
Michael <michaels3491@gmail.com>
2015-06-26 11:50:25 -0400

    adjusting the yard field available values and toJSON methods

--- src/values/field/Yard.js
+++ src/values/field/Yard.js
@@ -9,18 +9,23 @@ class YardField extends Field {
         super(field);
 
         let yard = {
-            content: !field.isRequired ? 'Optional' : undefined
+            content: !field.isRequired ? null : undefined,
+            name: !field.isRequired ? 'Optional' : 'Select'
         };
 
-        if (field.value) yard.content = field.value;
+        if (field.value) {
+            yard.content = Number(field.value);
+            yard.name = field.value;
+        }
 
         this.currentValue = yard;
 
         this.availableValues = [];
 
         this.availableValues = Array.from(new Array(99), (item, yardLength) => {
-            return {content: yardLength + 1};
+            return {content: yardLength + 1, name: yardLength + 1};
         });
+
     }
 
     get currentValue() {
@@ -29,15 +34,17 @@ class YardField extends Field {
 
     set currentValue(yard) {
         let value = {};
+        value.name = yard.name;
         value.content = yard.content;
         this.value = value;
     }
 
     toJSON() {
         let variableValue = {};
+        let value = (!this.isRequired && this.value.content === null) ? null : String(this.value.content);
         variableValue = {
             type: null,
-            value: this.value.content
+            value
         };
 
         return this.isValid(variableValue) ? JSON.stringify(variableValue) : 'Corrupted ' + this.inputType;
