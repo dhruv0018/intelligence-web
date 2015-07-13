40d20d111e1dcc14575e3b399943aef7e29e8980
Bryan Gislason <bryang@krossover.com>
2015-07-13 17:55:12 -0400

    Pill/Pill directives working on arena chart (shot chart)

--- app/games/arena-chart/index.js
+++ app/games/arena-chart/index.js
@@ -78,6 +78,8 @@ function GamesArenaChartData (
 GamesArenaChartController.$inject = [
     'Games.ArenaChart.Data',
     'ARENA_TYPES',
+    'CustomtagsFactory',
+    'PlayersFactory',
     'GamesFactory',
     'TeamsFactory',
     'LeaguesFactory',
@@ -89,6 +91,8 @@ GamesArenaChartController.$inject = [
 function GamesArenaChartController(
     data,
     ARENA_TYPES,
+    customTags,
+    players,
     games,
     teams,
     leagues,
@@ -99,8 +103,17 @@ function GamesArenaChartController(
 
     let game = games.get($stateParams.id);
     let team = teams.get(game.teamId);
+    let opposingTeam = teams.get(game.opposingTeamId);
     let league = leagues.get(team.leagueId);
     let arenaEvents = data.arenaEvents;
+    let customtags = customTags.getList();
+
+    let teamPlayersFilter = {rosterId: game.getRoster(game.teamId).id};
+    let teamPlayerList = players.getList(teamPlayersFilter);
+
+    let opposingTeamPlayersFilter = { rosterId: game.getRoster(game.opposingTeamId).id };
+    let opposingTeamPlayerList = players.getList(opposingTeamPlayersFilter);
+
 
     // Determine arena type
     try {
@@ -112,6 +125,29 @@ function GamesArenaChartController(
     $scope.arenaEvents = arenaEvents;
     $scope.filteredArenaEvents = [];
 
+    /* Construct pills */
+
+    let pills = [];
+
+    teamPlayerList.forEach((player) => {
+        let playerCopy = angular.copy(player);
+        playerCopy.name = player.shortName;
+        pills.push(playerCopy);
+    });
+
+    opposingTeamPlayerList.forEach((player) => {
+        let playerCopy = angular.copy(player);
+        playerCopy.name = player.shortName;
+        pills.push(playerCopy);
+    });
+
+    customtags.forEach((tag) => {
+        pills.push(tag);
+    });
+
+    $scope.pills = pills;
+
+    // TODO: Add custom tags to pills
 
     /* reset filters */
     $scope.resetFilters = function() {
@@ -120,12 +156,66 @@ function GamesArenaChartController(
     };
 
     let removeFiltersWatch = $scope.$watch('filters', filtersWatch, true);
+    let removeRemovedPillWatch = $scope.$watch('removedPill', removedPillWatch);
     $scope.$on('$destroy', onDestroy);
 
     /* Filter arenaEvents in this watch to have access to the filtered results in this scope */
     function filtersWatch(newFilters) {
 
+        if (!newFilters) return;
+
+        /* Filter arena events */
         $scope.filteredArenaEvents = $filter('arenaEvents')($scope.arenaEvents, newFilters);
+
+        /* Setup Pills */
+        // player names, // custom tag names
+        $scope.pills = pills.filter((pill) => {
+
+            /* Team Players */
+            if (pill.model === 'PlayersResource') {
+
+                let isTeamPlayer = newFilters.teamPlayersIds.some((playerId) => {
+                    return pill.id === playerId;
+                });
+
+                if (isTeamPlayer) return isTeamPlayer;
+
+                let isOpposingTeamPlayer = newFilters.opposingTeamPlayersIds.some((playerId) => {
+                    return pill.id === playerId;
+                });
+
+                return isOpposingTeamPlayer;
+
+            } else if (pill.model === 'CustomtagsResource') {
+
+                let isCustomTag = newFilters.customTagIds.some((tagId) => {
+                    return pill.id === tagId;
+                });
+
+                return isCustomTag;
+            }
+        });
+    }
+
+    function removedPillWatch(pill) {
+
+        if (!pill) return;
+
+        if (pill.model === 'PlayersResource') {
+
+            let index = $scope.filters.teamPlayersIds.indexOf(pill.id);
+            if (index != -1) $scope.filters.teamPlayersIds.splice(index, 1);
+
+            index = $scope.filters.opposingTeamPlayersIds.indexOf(pill.id);
+            if (index != -1) $scope.filters.opposingTeamPlayersIds.splice(index, 1);
+
+        } else if (pill.model === 'CustomtagsResource') {
+
+            index = $scope.filters.customTagIds.indexOf(pill.id);
+            if (index != -1) $scope.filters.customTagIds.splice(index, 1);
+        }
+
+        $scope.removedPill = null;
     }
 
     function onDestroy() {
