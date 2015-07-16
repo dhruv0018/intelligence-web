8ce23b228ddfa15a8e6dfc8b134d4ec79b0259cf
Dave Krstin <david@krossover.com>
2015-07-16 05:13:53 -0400

    Added Static text Field class and unit tests.

---
+++ src/values/field/Static.js
@@ -0,0 +1,44 @@
+import Field from './Field.js';
+
+class StaticField extends Field {
+
+    constructor (field) {
+
+        if (!field && !field.value) {
+
+            throw new Error('StaticField constructor requires a field!');
+        }
+
+        super(field);
+
+        this.currentValue = field.value;
+    }
+
+    get currentValue () {
+
+        return this.value;
+    }
+
+    set currentValue (text) {
+
+        this.value = text;
+    }
+
+    /**
+     * Method: toString
+     * Generates an HTML string of the field.
+     *
+     * @return: {String} HTML of the field
+     */
+    toString () {
+
+        return `<span class="static static-field">${this.currentValue}</span>`;
+    }
+
+    toJSON () {
+
+        return this.currentValue;
+    }
+}
+
+export default StaticField;
