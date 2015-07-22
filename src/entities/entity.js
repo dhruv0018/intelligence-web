fbc990e93ad9dbf1a57d3e9e888b446b3a9bb827
Dave Krstin <david@krossover.com>
2015-07-22 00:39:18 -0400

    Added JSDoc comments to base entity class.

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -1,6 +1,13 @@
-
+/**
+ * Entity Model
+ * @class Entity
+ */
 class Entity {
 
+    /**
+     * @constructs Entity
+     * @param {Object} entity - JSON from server
+     */
     constructor (entity) {
 
         Object.assign(this, entity);
