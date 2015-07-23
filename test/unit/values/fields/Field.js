f6b57b06b75f014347f4b70305227abdd180e3e8
Dave Krstin <david@krossover.com>
2015-07-23 19:51:33 -0400

    Updated test cases for base Field class.

--- test/unit/values/fields/Field.js
+++ test/unit/values/fields/Field.js
@@ -11,6 +11,7 @@ const rawVariableValue = FieldData;
 describe('Generic Field', () => {
 
     let field;
+    let testString = 'Test String.';
 
     beforeEach(angular.mock.module('intelligence-web-client'));
 
@@ -18,12 +19,40 @@ describe('Generic Field', () => {
         field = new Field(rawVariableValue);
     });
 
-    it('The Field Class should Exist', () => {
+    it('should Exist', () => {
 
         expect(Field).to.exist;
     });
 
-    it('The Field Class has appropriate properties expected from the model', () => {
+    it('should have public API', () => {
+
+        expect(Field).to.respondTo('initialize');
+        expect(Field).to.respondTo('toString');
+        expect(Field).to.respondTo('toJSON');
+    });
+
+    it('should have a getter, "currentValue", for the "value" property.', () => {
+
+        field.value = testString;
+
+        expect(field.currentValue).to.equal(testString);
+    });
+
+    it('should have a setter, "currentValue", for the "value" property.', () => {
+
+        field.currentValue = testString;
+
+        expect(field.value).to.equal(testString);
+    });
+
+    it('the "initialize" method should sets the value property', () => {
+
+        field.initialize(testString);
+
+        expect(field.currentValue).to.equal(testString);
+    });
+
+    it('has appropriate properties expected from the model', () => {
         assert.isDefined(field.id, 'The id property is defined');
         assert.isDefined(field.type, 'The type property is defined');
         assert.isDefined(field.isRequired, 'The isRequired property is defined');
