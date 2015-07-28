8e96d9c064bfeb1d9b426038d87ffc224629ddda
Alex Young <alexyoung1992@gmail.com>
2015-07-27 23:36:43 -0400

    Documentation for UIEventEmitter

--- src/emitters/ui-event.js
+++ src/emitters/ui-event.js
@@ -1,15 +1,34 @@
+/* Import Angular IntelligenceWebClient module */
 const pkg = require('../../package.json');
 const angular = window.angular;
 const IntelligenceWebClient = angular.module(pkg.name);
+
+/* Import EventEmitter Node module */
 const EventEmitter = require('events').EventEmitter;
 
+/* Import EVENT types constant */
 import EVENT from '../constants/event';
 
+/* UIEvent > KeyboardEvent keycodes */
 const ENTER_KEY_CODE = 13;
 const ESC_KEY_CODE = 27;
 
+/**
+ * A Node EventEmitter that broadcasts HTML UIEvents
+ * @class UIEventEmitter
+ * @extends EventEmitter
+ * @example
+ * import uiEventEmitter from 'src/emitters/ui-event';
+ * import EVENT from 'src/constants/event';
+ * uiEventEmitter.on(EVENT.UI.KEY_DOWN.ENTER, event => {...});
+ */
 class UIEventEmitter extends EventEmitter {
 
+    /**
+     * Binds desired GlobalEventHandlers to class methods
+     * @constructor
+     * @returns {UIEventEmitter}
+     */
     constructor () {
 
         super();
@@ -17,6 +36,11 @@ class UIEventEmitter extends EventEmitter {
         window.onkeydown = event => this.onKeyDown(event);
     }
 
+    /**
+     * GlobalEventHandlers.onkeydown callback
+     * @method UIEventEmitter.onKeyDown
+     * @param {UIEvent} event
+     */
     onKeyDown (event) {
 
         switch (event.keyCode) {
@@ -34,8 +58,11 @@ class UIEventEmitter extends EventEmitter {
     }
 }
 
+/* Singleton UIEventEmitter */
 const uiEventEmitter = new UIEventEmitter();
 
+/* Open singleton to Angular DI */
 IntelligenceWebClient.factory('UIEventEmitter', () => uiEventEmitter);
 
+/* Export singleton for ES6, non-Angular modules */
 export default uiEventEmitter;
