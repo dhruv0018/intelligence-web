b773352e6f1527d0980f97f8bf0f31de3b0d9f9b
Michael <michaels3491@gmail.com>
2015-08-03 17:13:19 -0400

    updating yard field and yard unit tests

--- test/unit/values/sample-data/Yard.js
+++ test/unit/values/sample-data/Yard.js
@@ -1,25 +1,12 @@
-let YardFieldData = {};
-
-YardFieldData.Tag = {
-    formations: [],
-    id: 1,
-    index: 1,
-    type: 'YARD',
-    isRequired: true,
-    options: [],
-    type: null
-};
-
-YardFieldData.Event = {
+let field = {
     formations: [],
     id: 1,
     index: 1,
     type: 'YARD',
     isRequired: true,
     options: [1,2,3],
-    type: null,
     value: '1'
 };
 
 
-export default YardFieldData;
+export default field;
