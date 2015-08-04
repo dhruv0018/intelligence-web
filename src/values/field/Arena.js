0234e1b5785ae3d829f869dad45d75f43bf8d367
Michael <michaels3491@gmail.com>
2015-08-04 11:44:08 -0400

    updating arena model and directive to use field.value

--- src/values/field/Arena.js
+++ src/values/field/Arena.js
@@ -17,84 +17,27 @@ class ArenaField extends Field {
         if (!field) return;
         super(field);
 
+        let region = this.initializeValue(field.value.region);
+        let coordinates = {
+            x: this.initializeValue(field.value.coordinates.x),
+            y: this.initializeValue(field.value.coordinates.y)
+        };
 
-        this.initialize();
-
-        Object.defineProperty(this.value, 'name', {
-            get: () => {
+        let value = {
+            region,
+            coordinates,
+            get name() {
                 let calculatedName = !this.isRequired ? 'Optional' : this.name;
-                if (this.region) {
+                if (region) {
                     let injector = angular.element(document).injector();
                     this.regionMap = injector.get('ARENA_REGIONS_BY_ID');
                     calculatedName = angular.copy(this.regionMap[this.region].name);
                 }
                 return calculatedName;
             }
-        });
-        this.availableValues = null;
-    }
-
-    //TODO temporary a NEED
-    arenaName(region = this.value.region){
-        let calculatedName = !this.isRequired ? 'Optional' : this.name;
-        if (region) {
-            let injector = angular.element(document).injector();
-            this.regionMap = injector.get('ARENA_REGIONS_BY_ID');
-            calculatedName = angular.copy(this.regionMap[region].name);
-        }
-        return calculatedName;
-    }
-    /**
-     * Sets the value property by creating an 'available value'. If called from
-     * the constructor, it uses default value if none are passed in.
-     *
-     * @method initialize
-     * @param {integer} [value] - the value to be set
-     * @returns {undefined}
-     */
-    initialize (value = this.value) {
-
-        //todo look into initialization of arena value
-        let arena = {
-            region: !this.isRequired ? null : undefined,
-            coordinates: {
-                x: !this.isRequired ? null: undefined,
-                y: !this.isRequired ? null: undefined
-            },
-            name: this.name
         };
 
-        if (value && value.coordinates) {
-            arena.coordinates = value.coordinates;
-            arena.region = value.region;
-            arena.name = this.arenaName(arena.region) || this.name;
-        }
-
-        this.currentValue = arena;
-    }
-
-    get currentValue () {
-
-        return this.value;
-    }
-
-    set currentValue(arena) {
-        let value = {};
-        value.coordinates = arena.coordinates;
-        value.region = arena.region && arena.region.id ? arena.region.id : arena.region;
         this.value = value;
-    }
-
-
-    /**
-     * Generates an HTML string of the field.
-     *
-     * @method toString
-     * @returns {String} - HTML of the field
-     */
-    toString () {
-        let name = this.arenaName();
-        return `<span class="value gap-field">${name}</span>`;
     }
 
     /**
