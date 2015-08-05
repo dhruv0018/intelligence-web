8793620dbb606088aa362d8dacde451f95013024
Tory Mance <tory@krossover.com>
2015-08-05 11:58:07 -0400

    Create new basic-edit-profile info for non recruits

--- app/athlete/film-home/controller.js
+++ app/athlete/film-home/controller.js
@@ -1,11 +1,11 @@
 /* Fetch angular from the browser scope */
-var angular = window.angular;
+const angular = window.angular;
 
 /**
  * FilmHome page module.
  * @module FilmHome
  */
-var FilmHome = angular.module('Athlete.FilmHome');
+const FilmHome = angular.module('Athlete.FilmHome');
 
 /**
  * FilmHome controller.
@@ -17,21 +17,19 @@ FilmHome.controller('Athlete.FilmHome.controller', [
     '$scope', 'SessionService', 'TeamsFactory', 'GamesFactory', 'PlayersFactory', 'UsersFactory', 'Athlete.FilmHome.Data', 'ReelsFactory',
     function controller($scope, session, teams, games, players, users, data, reels) {
 
-        var teamId = session.currentUser.currentRole.teamId;
+        let teamId = session.currentUser.currentRole.teamId;
 
         $scope.playersList = data.playersList;
         $scope.team = teams.get(teamId);
         $scope.teams = teams.getCollection();
         $scope.games = games.getCollection();
         $scope.reels = reels.getCollection();
-        var gamesList = games.getByRelatedRole();
-        var reelsList = reels.getByRelatedRole();
+        let gamesList = games.getByRelatedRole();
+        let reelsList = reels.getByRelatedRole();
         $scope.filmsList = gamesList.concat(reelsList);
         $scope.query = '';
-        $scope.athlete = {
-            user: users.get(session.currentUser.id),
-            players: players.getList()
-        };
+        $scope.currentUser = users.get(session.currentUser.id);
+        $scope.userPlayers = players.getList();
 
         //ui
         $scope.filteredFilmsList = $scope.filmsList;
