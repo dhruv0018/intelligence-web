2fd9d8a6b26cbe64f03dffb16f94203985451bc1
Tory Mance <tory@krossover.com>
2015-08-07 11:53:57 -0400

    Control more from clips nav

--- app/athlete/profile/highlights/controller.js
+++ app/athlete/profile/highlights/controller.js
@@ -50,42 +50,14 @@ function HighlightsController (
         if ($scope.featuredReel) {
 
             // Populate the array with play objects from playIds
-            let playsArray = $scope.featuredReel.plays.map(function(playId) {
+            $scope.playsArray = $scope.featuredReel.plays.map(function(playId) {
                 return plays.get(playId);
             });
 
             /* Load the plays in the plays manager
              * TODO: accomplish this without playsManager
              */
-            playsManager.reset(playsArray);
-
-            // Start with first play
-            $scope.currentPlay = playsArray[0];
-            $scope.sources = $scope.currentPlay.getVideoSources();
-            $scope.clipTotal = $scope.featuredReel.plays.length;
-            $scope.clipIndex = playsManager.getIndex($scope.currentPlay) + 1;
-
-            $scope.$watch('clipIndex', function updatePlayInfo() {
-                // When clip index is changed, adjust adjacent plays accordingly
-                $scope.previousPlay = playsManager.getPreviousPlay($scope.currentPlay);
-                $scope.nextPlay = playsManager.getNextPlay($scope.currentPlay);
-            });
-        }
-
-        // When clip finishes playing, go to next play if continuous play is on
-        VideoPlayerEventEmitter.on(VIDEO_PLAYER_EVENTS.ON_CLIP_COMPLETE, onCompleteVideo);
-
-        function onCompleteVideo() {
-
-            /* If continuous play is on. */
-            if (playManager.playAllPlays) {
-                $scope.goToPlay($scope.nextPlay);
-
-                VideoPlayerEventEmitter.on(VIDEO_PLAYER_EVENTS.ON_CAN_PLAY, function playVideo() {
-                    videoPlayer.play();
-                    VideoPlayerEventEmitter.removeListener(VIDEO_PLAYER_EVENTS.ON_CAN_PLAY, playVideo);
-                });
-            }
+            playsManager.reset($scope.playsArray);
         }
 
         $scope.highlightReels = [];
