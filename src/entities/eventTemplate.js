01acbed3241ee760d53c2bfed7910b567225eb84
Michael <michaels3491@gmail.com>
2015-08-11 15:07:15 -0400

    abstracting the event id into a variable

--- src/entities/eventTemplate.js
+++ src/entities/eventTemplate.js
@@ -8,10 +8,11 @@
  */
 
 export default (event, scriptHtmlString) => {
+    let domId = event.id ? 'select-event-' + event.id + '-cta' : 'select-event-new-cta';
     return `
     <li class="event">
 
-        <button id="${ event.id ? 'select-event-' + event.id + '-cta' : 'select-event-new-cta'}" class="item btn-select-event" ng-click="selectEvent(${event.id});">
+        <button id="${domId}" class="item btn-select-event" ng-click="selectEvent(${event.id});">
 
             ${scriptHtmlString}
 
