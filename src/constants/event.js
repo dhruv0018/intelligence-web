aee3544879c0bcd90f8c135fc38a8726c43f1145
Alex Young <alexyoung1992@gmail.com>
2015-08-24 15:33:43 -0400

    feature(event complilation): create INDEXING_EVENT_COMPLETE event to trigger script complilation
    Previously was emitting PLAYLIST_EVENT_CURRENT_CHANGE which was
    an inappropriate and potentially causing handler collisions

--- src/constants/event.js
+++ src/constants/event.js
@@ -8,24 +8,33 @@ var IntelligenceWebClient = angular.module(pkg.name);
 const EVENT = {
 
     PLAYLIST: {
+
         PLAY: {
             WATCH: 'PLAYLIST_PLAY_WATCH',
             SELECT: 'PLAYLIST_PLAY_SELECT',
             CURRENT: 'PLAYLIST_PLAY_CURRENT'
         },
+
         EVENT: {
             SELECT: 'PLAYLIST_EVENT_SELECT',
             CURRENT_CHANGE: 'PLAYLIST_EVENT_CURRENT_CHANGE'
         },
+
         PLAYS: {
             CALCULATE: 'PLAYLIST_PLAYS_CALCULATE'
+        }
+    },
+
+    INDEXING: {
+
+        EVENT: {
+            COMPLETE: 'INDEXING_EVENT_COMPLETE'
         }
     },
 
     UI: {
 
         KEY_DOWN: {
-
             ENTER: 'ENTER_KEY_DOWN',
             ESC: 'ESC_KEY_DOWN'
         }
