6c32ab9bfe4ae2aa8dc1c22ac7f5c68e9304a1e7
Dave Krstin <david@krossover.com>
2015-08-25 14:17:17 -0400

    fix(Reset Password): Update reset password error link
    Update reset password error link.

--- app/login/index.js
+++ app/login/index.js
@@ -527,7 +527,7 @@ function LoginController(
 
                     alerts.add({
                         type: 'danger',
-                        message: 'Sorry, the link is no longer valid. <a href="/intelligence/login">Click here</a> to reset your password again.'
+                        message: 'Sorry, the link is no longer valid. Click <a href="/intelligence/login">here</a> to reset your password again.'
                     });
 
                     throw new Error('Could not reset password');
