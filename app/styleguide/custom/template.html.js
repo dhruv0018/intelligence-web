f7df3e888b42f861c8fea02c1e0a02a8cd42c32a
Alex Young <alexyoung1992@gmail.com>
2015-08-31 07:13:53 -0400

    feat(styleguide): add arenas to custom components

--- app/styleguide/custom/template.html.js
+++ app/styleguide/custom/template.html.js
@@ -4,5 +4,21 @@ export default `
 
         <h2>Custom Krossover Components</h2>
 
+        <div class="arenas">
+
+            <h3>Arenas</h3>
+
+            <select
+                ng-options="arenaId as arena.name for (arenaId, arena) in ARENA_TYPES"
+                ng-model="selectedArenaId"
+                ng-change="selectedArenaType = ARENA_TYPES[selectedArenaId].type"
+            ></select>
+
+            <krossover-arena
+                type="selectedArenaType"
+            ></krossover-arena>
+
+        </div>
+
     </div>
 `;
