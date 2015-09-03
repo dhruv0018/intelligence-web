b54880385bc31af6ffdbaf5e0b3467db2882f1df
Bryan Gislason <bryang@krossover.com>
2015-09-02 21:48:52 -0400

    chore(styleguide - dialogs): Add rest of the dialogs to the styleguide except for Basic ones

--- app/styleguide/dialogs/controller.js
+++ app/styleguide/dialogs/controller.js
@@ -3,6 +3,20 @@ const angular = window.angular;
 
 StyleguideDialogsController.$inject = [
     'PlaysFactory',
+    'AddProfileTeam.Modal',
+    'AddReel.Modal',
+    'AdminManagement.Modal',
+    'ChangePassword.Modal',
+    'DeleteGame.Modal',
+    'ExcelUpload.Modal',
+    'AssistantInfo.Modal',
+    'AthleteInfo.Modal',
+    'kvsUploaderInterface.Modal',
+    'ManageProfileReels.Modal',
+    'ProfileOnboarding.Modal',
+    'RawFilm.Modal',
+    'RolesModal',
+    'SelectIndexer.Modal',
     'ArenaDialog.Service',
     'MobileAppDialog.Service',
     'TermsDialog.Service',
@@ -13,6 +27,20 @@ StyleguideDialogsController.$inject = [
 
 function StyleguideDialogsController (
     playsFactory,
+    AddProfileTeamModal,
+    AddReelModal,
+    AdminManagementModal,
+    ChangePasswordModal,
+    DeleteGameModal,
+    ExcelUploadModal,
+    AssistantInfoModal,
+    AthleteInfoModal,
+    kvsUploaderInterfaceModal,
+    ManageProfileReelsModal,
+    ProfileOnboardingModal,
+    RawFilmModal,
+    RolesModal,
+    SelectIndexerModal,
     ArenaDialog,
     MobileAppDialog,
     TermsDialog,
@@ -23,35 +51,141 @@ function StyleguideDialogsController (
 
     const dialogs = [
         {
-            name: 'Arena Field Dialog (Not Ready, requires dependencies)',
+            name: 'Arena Field',
             controller: ArenaDialog,
+            enabled: false,
+            type: 'Material Dialog',
             args: []
         },
         {
-            name: 'Mobile App Dialog',
+            name: 'Add Profile Team',
+            controller: AddProfileTeamModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: []
+        },
+        {
+            name: 'Add Reel',
+            controller: AddReelModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: []
+        },
+        {
+            name: 'Admin Management',
+            controller: AdminManagementModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: []
+        },
+        {
+            name: 'Change password',
+            controller: ChangePasswordModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: []
+        },
+        {
+            name: 'Delete Game',
+            controller: DeleteGameModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: []
+        },
+        {
+            name: 'Excel Upload',
+            controller: ExcelUploadModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: []
+        },
+        {
+            name: 'Assistant Info',
+            controller: AssistantInfoModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: []
+        },
+        {
+            name: 'Athlete Info',
+            controller: AthleteInfoModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: []
+        },
+        {
+            name: 'KVS Uploader Interface',
+            controller: kvsUploaderInterfaceModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: []
+        },
+        {
+            name: 'Manage Profile Reels ',
+            controller: ManageProfileReelsModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: []
+        },
+        {
+            name: 'Profile Onboarding',
+            controller: ProfileOnboardingModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: []
+        },
+        {
+            name: 'Select Indexer',
+            controller: SelectIndexerModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: [null, false]
+        },
+        {
+            name: 'Raw Film',
+            controller: RawFilmModal,
+            type: 'UI-Bootstrap modal',
+            enabled: false,
+            args: [null]
+        },
+        {
+            name: 'Roles',
+            controller: RolesModal,
+            type: 'UI-Bootstrap modal',
+            enabled: true,
+            args: []
+        },
+        {
+            name: 'Mobile App',
             controller: MobileAppDialog,
+            type: 'Material Dialog',
+            enabled: true,
             args: []
         },
         {
-            name: 'Terms Dialog',
+            name: 'Terms',
             controller: TermsDialog,
+            type: 'Material Dialog',
+            enabled: true,
             args: []
         },
         {
-            name: 'Breakdown Dialog (Not Ready, requires loaded or mock Data)',
+            name: 'Breakdown',
             controller: BreakdownDialog,
+            type: 'Material Dialog',
+            enabled: false,
             args: []
         }
     ];
 
     $scope.dialogs = dialogs;
 
-    $scope.showDialog = ($event, index) => {
+    $scope.show = ($event, dialog) => {
 
-        let dialog = dialogs[index];
         let args = dialog.args;
 
-        dialog.controller.show($event, ...args);
+        if (dialog.type === 'Material Dialog') dialog.controller.show($event, ...args);
+        else if (dialog.type === 'UI-Bootstrap modal') dialog.controller.open(...args);
     };
 }
 
--- app/styleguide/dialogs/template.js
+++ app/styleguide/dialogs/template.js
@@ -3,17 +3,41 @@ export default `
     <div class="dialogs">
 
         <h3> Generic Examples </h3>
-
-            <p> TODO </p>
+        <p> TODO </p>
 
         <h3> Real Examples </h3>
+        <h4> TODO: </h4>
+        <ul style="color: red">
+            <li>Refactor Dialogs that can't display without the data they need to function completely (i.e at least viewable)</li>
+            <li>Add Basic Modal Examples</li>
+        </ul>
 
-        <ul>
+        <table class="table">
 
-            <li ng-repeat="dialog in dialogs">
-                <label> {{ dialog.name }} </label>
-                <button ng-click="showDialog($event, $index)">Show</button>
-            </li>
+            <tr>
+                <th>Dialog Description</th>
+                <th>Type</th>
+                <th>Show</th>
+                <th>Info</th>
+            </tr>
+
+            <tr ng-repeat="dialog in dialogs | orderBy: ['-enabled', 'name']">
+
+                <td>
+                    <span ng-if="dialog.enabled">{{ dialog.name }}</span>
+                    <span ng-if="!dialog.enabled" style="color: red"><em>{{ dialog.name }}</em></span>
+                </td>
+
+                <td>{{dialog.type}}</td>
+
+                <td>
+                    <button ng-click="show($event, dialog)">Show</button>
+                </td>
+
+                <td>
+                    <span ng-if="!dialog.enabled">(Not ready, needs refactor or dependencies)</span>
+                </td>
+            </tr>
 
 
         </ul>
