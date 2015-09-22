32e4483d6b716a7ca99b3099c0294475af1b8fe8
Dave Krstin <david@krossover.com>
2015-09-22 12:40:21 -0400

    test(Tag Entity): Add toJSON test
    Add toJSON test for Tag Entity.

--- test/unit/entities/tag.js
+++ test/unit/entities/tag.js
@@ -104,8 +104,37 @@ describe('Tag Entity', () => {
         assert(tag.toJSON.should.have.been.called);
     });
 
-    it('should throw an error on a JSON.stringify call', () => {
+    it('should restore the original JSON on JSON.stringify calls', () => {
 
-        expect(() => JSON.stringify(tag)).to.throw(Error);
+        tag = tag.toJSON();
+
+        expect(tag.id).to.equal(srcTag.id);
+        expect(tag.name).to.equal(srcTag.name);
+        expect(tag.indexerScript).to.equal(srcTag.indexerScript);
+        expect(tag.userScript).to.equal(srcTag.userScript);
+        expect(tag.shortcutKey).to.equal(srcTag.shortcutKey);
+        expect(tag.description).to.equal(srcTag.description);
+        expect(tag.isStart).to.equal(srcTag.isStart);
+        expect(tag.isEnd).to.equal(srcTag.isEnd);
+        expect(tag.tagSetId).to.equal(srcTag.tagSetId);
+        expect(tag.children).to.deep.equal(srcTag.children);
+
+        expect(tag.tagVariables).to.be.an.array;
+        tag.tagVariables.forEach((variable, index) => {
+
+            expect(variable.id).to.equal(srcTag.tagVariables[index].id);
+            expect(variable.name).to.equal(srcTag.tagVariables[index].name);
+            expect(variable.type).to.equal(srcTag.tagVariables[index].type);
+            expect(variable.isRequired).to.equal(srcTag.tagVariables[index].isRequired);
+            expect(variable.options).to.deep.equal(srcTag.tagVariables[index].options);
+            expect(variable.formations).to.deep.equal(srcTag.tagVariables[index].formations);
+        });
+
+        expect(tag.pointsAssigned).to.equal(srcTag.pointsAssigned);
+        expect(tag.assignThisTeam).to.equal(srcTag.assignThisTeam);
+        expect(tag.isPeriodTag).to.equal(srcTag.isPeriodTag);
+        expect(tag.summaryScript).to.equal(srcTag.summaryScript);
+        expect(tag.summaryPriority).to.equal(srcTag.summaryPriority);
+        expect(tag.buffer).to.equal(srcTag.buffer);
     });
 });
