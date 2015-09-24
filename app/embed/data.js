2d4cac2f6bf5ec1049572cba0164009ce849ae65
Tory Mance <tory@krossover.com>
2015-09-24 13:46:50 -0400

    refactor(Embed): don't load unecessary data on embed page

--- app/embed/data.js
+++ app/embed/data.js
@@ -26,13 +26,9 @@ function EmbedDataDependencies (
         constructor (reelId) {
 
             /* Load data. */
-            this.tagset = tagsets.load();
-            this.leagues = leagues.load();
             this.reel = reels.load(reelId);
             this.games = games.load({reelId: reelId});
-            this.teams = teams.load({reelId: reelId});
             this.plays = plays.load({reelId: reelId});
-            this.players = players.load({reelId: reelId});
         }
     }
 
