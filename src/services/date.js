4a20654883a65ce467dec6ad199f3ba552b77f54
Dave Krstin <david@krossover.com>
2015-09-28 17:11:22 -0400

    fix(NewDate Service): Prevent date discrepancy
    Simplify start and end start creation for plans and packages. Only
    adjust times to midnight for new dates; leave existing dates alone.

--- src/services/date.js
+++ src/services/date.js
@@ -12,29 +12,40 @@ IntelligenceWebClient.factory('NewDate', function() {
 
     momentTimezone.tz.add('America/New_York|EST EDT EWT EPT|50 40 40 40|01010101010101010101010101010101010101010101010102301010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-261t0 1nX0 11B0 1nX0 11B0 1qL0 1a10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 RB0 8x40 iv0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 s10 1Vz0 LB0 1BX0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0');
 
-    function dateString(existingDate) {
-        var dateZeroTime = (existingDate) ? new Date(existingDate) : new Date();
-        var dateStringVal = dateZeroTime.getFullYear() + '-' + (dateZeroTime.getMonth() + 1) + '-' + dateZeroTime.getDate();
-        return dateStringVal;
-    }
-
     return {
         //start day at 0 EST
         //end day at 11:59:59 EST
         generateNow: function() {
             return new Date();
         },
-        generatePlanStartDate: function(existingDate) {
-            var dateStringStartTime = dateString(existingDate) + ' ' + '00:00:00';
+        generatePlanStartDate: function (existingDate) {
 
-            var start = moment.utc(momentTimezone.tz(dateStringStartTime, 'America/New_York').format()).format();
-            return new Date(start);
+            /* Get tomorrow's date at midnight */
+            let start = momentTimezone.tz(existingDate, 'America/New_York');
+
+            if (!existingDate) {
+
+                start
+                    .startOf('day')
+                    .add(1, 'day');
+            }
+
+            return start.toDate();
         },
-        generatePlanEndDate: function(existingDate) {
-            var dateStringEndTime = dateString(existingDate) + ' ' + '23:59:59';
+        generatePlanEndDate: function (existingDate) {
 
-            var end = moment.utc(momentTimezone.tz(dateStringEndTime, 'America/New_York').format()).format();
-            return new Date(end);
+            /* Get The day after tomorrow just before midnight */
+            let end = momentTimezone.tz(existingDate, 'America/New_York');
+
+            if (!existingDate) {
+
+                end
+                    .startOf('day')
+                    .add(3, 'day')
+                    .subtract(1, 'milliseconds');
+            }
+
+            return end.toDate();
         }
     };
 });
