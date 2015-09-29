b26b5f67ad7e4b1d1462c6543e26fc41fb0022ce
Dave Krstin <david@krossover.com>
2015-09-29 18:36:13 -0400

    test(New Date Factory): Add tests
    Add tests for factory methods.

--- test/unit/factories/new-date.js
+++ test/unit/factories/new-date.js
@@ -1,3 +1,5 @@
+const momentTimezone = require('moment-timezone');
+
 const assert  = chai.assert;
 const expect  = chai.expect;
 const should  = chai.should();
@@ -10,4 +12,73 @@ describe('NewDate Factory', () => {
 
         expect(NewDate).to.exist;
     }));
+
+    it('should have public API', inject(NewDate => {
+
+        expect(NewDate).to.respondTo('generateNow');
+        expect(NewDate).to.respondTo('generatePlanStartDate');
+        expect(NewDate).to.respondTo('generatePlanEndDate');
+    }));
+
+    describe('generateNow method', () => {
+
+        it('return a Date', inject(NewDate => {
+
+            let now = NewDate.generateNow();
+
+            expect(now).to.be.an.instanceof(Date);
+        }));
+    });
+
+    describe('generatePlanStartDate method', () => {
+
+        it('return a Date', inject(NewDate => {
+
+            let startDate = NewDate.generatePlanStartDate();
+
+            expect(startDate).to.be.an.instanceof(Date);
+        }));
+
+        it('return tomorrow at midnight if no existing date is passed in', inject(NewDate => {
+
+            let startDate  = NewDate.generatePlanStartDate();
+            let todaysDate = new Date();
+
+            expect(startDate.getMonth()).to.equal(todaysDate.getMonth());
+            expect(startDate.getDate()).to.equal(todaysDate.getDate() + 1);
+            expect(startDate.getHours()).to.equal(0);
+            expect(startDate.getMinutes()).to.equal(0);
+            expect(startDate.getSeconds()).to.equal(0);
+            expect(startDate.getMilliseconds()).to.equal(0);
+        }));
+    });
+
+    describe('generatePlanEndDate method', () => {
+
+        it('return a Date', inject(NewDate => {
+
+            let endDate = NewDate.generatePlanEndDate();
+
+            expect(endDate).to.be.an.instanceof(Date);
+        }));
+
+        it('return the day after tomorrow, just before midnight if no existing date is passed in', inject(NewDate => {
+
+            let endDate     = NewDate.generatePlanEndDate();
+            let controlDate = momentTimezone(undefined, 'America/New_York').add(2, 'days');
+
+            /* FIXME: Although this works, we should find a better way to get a
+             * 'control date' to test against. Aside from the time, the date
+             * above is calculated almost exactly as it is in the factory (using
+             * moment.js). The challenge here is to determine the month and day
+             * two days out that avoids excessive calculation that shouldn't be
+             * in the unit tests. */
+            expect(endDate.getMonth()).to.equal(controlDate.month());
+            expect(endDate.getDate()).to.equal(controlDate.date());
+            expect(endDate.getHours()).to.equal(23);
+            expect(endDate.getMinutes()).to.equal(59);
+            expect(endDate.getSeconds()).to.equal(59);
+            expect(endDate.getMilliseconds()).to.equal(999);
+        }));
+    });
 });
