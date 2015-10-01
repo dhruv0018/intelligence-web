b6f3fcf2358929b9419835ed022e4c1fb383160c
Dave Krstin <david@krossover.com>
2015-10-01 12:28:42 -0400

    refactor(NewDate Factory): DRY up new date construction
    DRY up new date construction.

--- src/services/date.js
+++ src/services/date.js
@@ -12,20 +12,25 @@ IntelligenceWebClient.factory('NewDate', function() {
 
     momentTimezone.tz.add('America/New_York|EST EDT EWT EPT|50 40 40 40|01010101010101010101010101010101010101010101010102301010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-261t0 1nX0 11B0 1nX0 11B0 1qL0 1a10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 RB0 8x40 iv0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 s10 1Vz0 LB0 1BX0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0');
 
+    function createDateFromISOString (ISODateString) {
+
+        return momentTimezone.tz(ISODateString, moment.ISO_8601, 'America/New_York');
+    }
+
     return {
         //start day at 0 EST
         //end day at 11:59:59 EST
         generateNow: function() {
             return new Date();
         },
-        generatePlanStartDate: function (existingDate) {
+        generatePlanStartDate: function (ISODateString) {
 
             let planStartDate;
 
-            if (existingDate !== undefined) {
+            if (ISODateString !== undefined) {
 
                 /* Attempt to create Date object from exisiting date */
-                planStartDate = momentTimezone.tz(existingDate, moment.ISO_8601, 'America/New_York');
+                planStartDate = createDateFromISOString(ISODateString);
             } else {
 
                 /* Get tomorrow's date at midnight */
@@ -36,18 +41,18 @@ IntelligenceWebClient.factory('NewDate', function() {
 
             if (!planStartDate.isValid()) {
 
-                throw new Error(`NewDate Factory: Attempt to generate plan start date with invalid value (${existingDate})`);
+                throw new Error(`NewDate Factory: Attempt to generate plan start date with invalid value (${ISODateString})`);
             }
 
             return planStartDate.toDate();
         },
-        generatePlanEndDate: function (existingDate) {
+        generatePlanEndDate: function (ISODateString) {
 
             let planEndDate;
 
-            if (existingDate !== undefined) {
+            if (ISODateString !== undefined) {
 
-                planEndDate = momentTimezone.tz(existingDate, moment.ISO_8601, 'America/New_York');
+                planEndDate = createDateFromISOString(ISODateString);
             } else {
 
                 /* Get The day after tomorrow just before midnight */
@@ -59,7 +64,7 @@ IntelligenceWebClient.factory('NewDate', function() {
 
             if (!planEndDate.isValid()) {
 
-                throw new Error(`NewDate Factory: Attempt to generate plan end date with invalid value (${existingDate})`);
+                throw new Error(`NewDate Factory: Attempt to generate plan end date with invalid value (${ISODateString})`);
             }
 
             return planEndDate.toDate();
