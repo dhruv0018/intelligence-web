74f42d115119bdf97c697c8d7c4e41803e7ded2f
Michael <michaels3491@gmail.com>
2015-10-01 17:18:31 -0400

    refactor: updating eventlist unit tests

--- test/unit/collections/eventList.js
+++ test/unit/collections/eventList.js
@@ -2,16 +2,15 @@ import EventList from '../../../src/collections/eventList';
 
 const srcArray = [
     {
+        time: 4252.4299316406
+    },
+    {
         time: 3052.4299316406
     },
     {
         time: 2914.5400390625
     }
 ];
-
-const srcEvent = {
-    time: 1052.4299316406
-};
 
 describe('EventList', () => {
     let srcArrayCopy;
@@ -30,10 +29,9 @@ describe('EventList', () => {
     it('should be sorted by time', () => {
         let index = 0;
         let time = 0;
-        while(sampleList.next()) {
-            let event = sampleList.current;
-            expect(event.time).to.be.at.least(time);
-            time = event.time;
+        for (let event of sampleList) {
+            expect(event.value.time).to.be.at.least(time);
+            time = event.value.time;
         }
     });
 });
