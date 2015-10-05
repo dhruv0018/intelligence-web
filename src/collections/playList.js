b36914d6931ce6746fe5f272339325aea7fd2865
Michael <michaels3491@gmail.com>
2015-10-05 14:05:26 -0400

    docs: updating docs for playlist sortProperty

--- src/collections/playList.js
+++ src/collections/playList.js
@@ -7,7 +7,7 @@ class PlayList extends SortedList {
      *
      * @constructs PlayList
      * @param {Array} [array]        - Array to use as backing store
-     * @param {String} sortProperty  - Property to sort array by
+     * @param {String} [sortProperty]  - Property to sort array by
      * @param {Boolean} [descending] - Sort array descending/ascending
      */
     constructor(array, sortProperty = 'startTime', descending = true) {
