277e77bd9f5117492cecd41952dc55e2b462633e
Alex Young <alexyoung1992@gmail.com>
2015-10-10 22:52:26 -0400

    chore(styleguide): remove tags from styleguide

--- app/styleguide/indexing/template.html.js
+++
@@ -1,9 +0,0 @@
-export default `
-    <div class="indexing">
-        <h2>Indexing Components</h2>
-        <div id="indexing-block">
-            <h3>Indexing Block</h3>
-            <krossover-tags></krossover-tags>
-        </div>
-    </div>
-`;
