fb31a223c7eeae79c1a5ad079b0335e1b573b2a5
Michael <michaels3491@gmail.com>
2015-10-13 14:04:48 -0400

    refactor: using the existing time filter

--- app/styleguide/custom/controller.js
+++ app/styleguide/custom/controller.js
@@ -9,46 +9,23 @@ class StyleguideCustomController {
         $scope.selectedArenaId = '1';
         $scope.selectedArenaType = ARENA_TYPES[$scope.selectedArenaId].type;
 
-        //Event Time Adjustmentss
+        //Event Time Adjustments
         $scope.events = [
             {
-                time: 30,
-                get displayTime() {
-                    return displayTime(this.time);
-                }
+                time: 3800
             },
             {
-                time: 20,
-                get displayTime() {
-                    return displayTime(this.time);
-                }
+                time: 3700
             },
             {
-                time: 10,
-                get displayTime() {
-                    return displayTime(this.time);
-                }
+                time: 3599
             }
         ];
-
-        function displayTime(seconds) {
-            const h = 3600;
-            const m = 60;
-            let hours = Math.floor(seconds/h);
-            let minutes = Math.floor( (seconds % h)/m );
-            let scnds = Math.floor( (seconds % m) );
-            let timeString = '';
-            if(scnds < 10) scnds = '0'+scnds;
-            if(hours < 10) hours = '0'+hours;
-            if(minutes < 10) minutes = '0'+minutes;
-            timeString = hours +':'+ minutes +':'+scnds;
-            return timeString;
-        }
 
         //mock game
         $scope.game = {
             video: {
-                duration: 40
+                duration: 4000
             }
         };
     }
