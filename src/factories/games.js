35b6fb216ee3baa99c0afe3a0796fafe472ea916
Akash <akash@krossover.com>
2015-10-13 18:32:33 -0400

    fix(basic game info on upload): Fix issue with home/away radio button always picking home radio butt

--- src/factories/games.js
+++ src/factories/games.js
@@ -81,7 +81,7 @@ IntelligenceWebClient.factory('GamesFactory', [
                 game.isSaving = false;
                 game.video = game.video ? new Video(game.video) : null;
                 game.notes = game.notes || {};
-                game.isHomeGame = game.isHomeGame || true;
+                game.isHomeGame = game.isHomeGame || false;
                 game.isDeleted = game.isDeleted || false;
                 game.datePlayed = game.datePlayed || moment.utc().toDate();
                 game.primaryJerseyColor = game.primaryJerseyColor || '#FFFFFF';
