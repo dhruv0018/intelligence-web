fc6f95a33c7c321c459df82ceb88f12c6860bc81
Michael <michaels3491@gmail.com>
2015-10-14 10:57:03 -0400

    feat: adding lower bound detection on playlist

--- src/collections/playList.js
+++ src/collections/playList.js
@@ -12,6 +12,34 @@ class PlayList extends SortedList {
      */
     constructor(array, sortProperty = 'startTime', descending = true) {
         super(array, sortProperty, descending);
+
+        //persistant state tracking iterator
+        this.statefulIterator = this.iterator();
+    }
+    /**
+     * Returns the start time of the preceeding play or null if there is no preceeding play
+     *
+     * @method lowerBoundingTime
+     * @returns {Integer | null}
+     */
+    lowerBoundingTime() {
+        let current = this.statefulIterator.current.value;
+        let previous = this.statefulIterator.readPrevious().value;
+        let time = null;
+        if (previous && previous.startTime) {
+            time = previous.startTime;
+        }
+        return time;
+    }
+
+    /**
+     * Returns the internal iterator
+     *
+     * @method playIterator
+     * @returns {Iterator}
+     */
+    get playIterator() {
+        return this.statefulIterator;
     }
 }
 
--- test/unit/collections/playList.js
+++ test/unit/collections/playList.js
@@ -6,6 +6,9 @@ const srcArray = [
     },
     {
         startTime: 2914.5400390625
+    },
+    {
+        startTime: 4242.2311313121
     }
 ];
 
@@ -30,5 +33,24 @@ describe('PlayList', () => {
             expect(play.value.startTime).to.be.at.least(startTime);
             startTime = play.value.startTime;
         }
+    });
+
+    describe('lowerBoundingTime', ()=> {
+
+        it('should return the time of the previous play when there is one', () => {
+            let index = sampleList.length - 1;
+            let previousIndex = index - 1;
+            sampleList.playIterator.current = sampleList.get(index);
+            let lowerBoundingTime = sampleList.lowerBoundingTime();
+            let expectedTime = sampleList.get(previousIndex).startTime;
+            expect(lowerBoundingTime).to.equal(expectedTime);
+        });
+
+        it('should return null if there is no previous play', () => {
+            let play = sampleList.first;
+            sampleList.playIterator.current = play;
+            let lowerBoundingTime = sampleList.lowerBoundingTime();
+            expect(lowerBoundingTime).to.be.null;
+        });
     });
 });
