88eb86b2f1860055c576023d726532bf679723e1
Michael <michaels3491@gmail.com>
2015-10-15 15:25:04 -0400

    feat: updating styleguide to account for event adjuster with respect to multiple plays

--- app/styleguide/custom/controller.js
+++ app/styleguide/custom/controller.js
@@ -9,23 +9,56 @@ class StyleguideCustomController {
         $scope.selectedArenaId = '1';
         $scope.selectedArenaType = ARENA_TYPES[$scope.selectedArenaId].type;
 
-        //Event Time Adjustments
-        $scope.events = [
+
+        $scope.plays = [
             {
-                time: 10.5
+                startTime: 5.5,
+                events: [
+                    {
+                        time: 5.5
+                    },
+                    {
+                        time: 7
+                    },
+                    {
+                        time: 8
+                    }
+                ]
             },
             {
-                time: 20
+                startTime: 10,
+                events: [
+                    {
+                        time: 11
+                    },
+                    {
+                        time: 20
+                    },
+                    {
+                        time: 30.5
+                    }
+                ]
             },
             {
-                time: 30.5
+                startTime: 40,
+                events: [
+                    {
+                        time: 41
+                    },
+                    {
+                        time: 45
+                    },
+                    {
+                        time: 47
+                    }
+                ]
             }
         ];
 
         //mock game
         $scope.game = {
             video: {
-                duration: 40
+                duration: 60
             }
         };
     }
--- app/styleguide/custom/template.html.js
+++ app/styleguide/custom/template.html.js
@@ -23,7 +23,7 @@ export default `
 
             <h4>Krossover Event Adjuster</h4>
 
-            <krossover-event-adjuster video="game.video" events="events" event="events[1]"></krossover-event-adjuster>
+            <krossover-event-adjuster plays="plays" play="plays[1]" video="game.video" events="plays[1].events" event="plays[1].events[0]"></krossover-event-adjuster>
         </div>
 
     </div>
