c7b038dfabfff3198814080e017b1ac3e9ffa916
Dave Krstin <david@krossover.com>
2015-11-03 21:13:49 -0500

    feat(constants): Add priorities to game queue views
    Add highest, high, and normal prioritiest to game views for admin queue.

--- src/constants/views.js
+++ src/constants/views.js
@@ -11,13 +11,39 @@ const IntelligenceWebClient = angular.module(pkg.name);
 const VIEWS = {
     QUEUE: {
         GAME: {
-            'status[]': [
-                GAME_STATUSES.READY_FOR_INDEXING.id,
-                GAME_STATUSES.INDEXING.id,
-                GAME_STATUSES.READY_FOR_QA.id,
-                GAME_STATUSES.QAING.id
-            ],
-            videoStatus: VIDEO_STATUSES.COMPLETE.id
+            PRIORITY_1: {
+                'status[]': [
+                    GAME_STATUSES.READY_FOR_INDEXING.id,
+                    GAME_STATUSES.INDEXING.id,
+                    GAME_STATUSES.READY_FOR_QA.id,
+                    GAME_STATUSES.QAING.id
+                ],
+                videoStatus: VIDEO_STATUSES.COMPLETE.id,
+                priority: 1,
+                orderBy: 'timeRemaining'
+            },
+            PRIORITY_2: {
+                'status[]': [
+                    GAME_STATUSES.READY_FOR_INDEXING.id,
+                    GAME_STATUSES.INDEXING.id,
+                    GAME_STATUSES.READY_FOR_QA.id,
+                    GAME_STATUSES.QAING.id
+                ],
+                videoStatus: VIDEO_STATUSES.COMPLETE.id,
+                priority: 2,
+                orderBy: 'timeRemaining'
+            },
+            PRIORITY_3: {
+                'status[]': [
+                    GAME_STATUSES.READY_FOR_INDEXING.id,
+                    GAME_STATUSES.INDEXING.id,
+                    GAME_STATUSES.READY_FOR_QA.id,
+                    GAME_STATUSES.QAING.id
+                ],
+                videoStatus: VIDEO_STATUSES.COMPLETE.id,
+                priority: 3,
+                orderBy: 'timeRemaining'
+            }
         },
         USERS: {
             'relatedGameStatus[]': [
