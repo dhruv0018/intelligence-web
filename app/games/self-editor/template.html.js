06bb5f2c4add1b819840ac6169b03f9ec5d26b23
Tory Mance <tory@krossover.com>
2015-11-13 10:37:32 -0500

    feat(Self Editor): strip down self-editor page

--- app/games/self-editor/template.html.js
+++ app/games/self-editor/template.html.js
@@ -2,24 +2,6 @@ export default `
 
     <div class="games-self-editor">
 
-        <div class="filter-container screen-lg-min">
-
-            <plays-filter game="game" ng-model="filteredPlaysIds">
-            </plays-filter>
-
-        </div>
-
-        <div class="main-highlight">
-
-            <krossover-sidebar></krossover-sidebar>
-
-            <video-player
-                class="screen-lg-min"
-                poster-image="posterImage"
-            ></video-player>
-
-        </div>
-
     </div>
 
 `;
