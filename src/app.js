b17b0d3c528e300d13862b15ee509c9789570055
Dave Krstin <david@krossover.com>
2015-11-19 17:09:08 -0500

    fix(App): Redirect pre-prod to new pre-prod URL
    Redirect pre-prod to new pre-prod URL.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ var pkg = require('../package.json');
 
 let host = window.location.host;
 
-if (
-    host === 'new.krossover.com' ||
-    host === 'v2-pre-prod.krossover.com'
-) {
+if (host === 'new.krossover.com') {
 
     document.location.assign('http://app.krossover.com/intelligence/login');
+} else if (host === 'v2-pre-prod.krossover.com') {
+
+    document.location.assign('https://v2-pre-prod-app.krossover.com/intelligence/login');
 }
 
 /* Fetch angular from the browser scope */
