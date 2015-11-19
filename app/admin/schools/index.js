51cb9e03010447211c64f2a6eb49daf4a181b521
Michael <michaels3491@gmail.com>
2015-11-19 11:01:41 -0500

    refactor(school-info): remove un-needed school save method

--- app/admin/schools/index.js
+++ app/admin/schools/index.js
@@ -116,12 +116,6 @@ Schools.controller('SchoolController', [
 
         const teamLeagues = new Map($scope.teams.map(team => [team.id, leagues.get(team.leagueId)]));
         $scope.teamLeagues = teamLeagues;
-
-        $scope.save = function(school) {
-            schools.save(school).then(function() {
-                $state.go('schools');
-            });
-        };
     }
 ]);
 
