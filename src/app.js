78f259deef83247a29d5b1746155dae6d4e75e32
Dave Krstin <david@krossover.com>
2015-11-23 16:09:23 -0500

    fix(App): Redirect to app.krossover.com with pathname
    Keep original pathname intact when redirecting.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ let host = window.location.host;
 
 if (host === 'new.krossover.com') {
 
-    document.location.assign('http://app.krossover.com/intelligence/login');
+    document.location.assign(`http://app.krossover.com${window.location.pathname}`);
 } else if (host === 'v2-pre-prod.krossover.com') {
 
-    document.location.assign('http://v2-pre-prod-app.krossover.com/intelligence/login');
+    document.location.assign(`http://v2-pre-prod-app.krossover.com${window.location.pathname}`);
 }
 
 /* Fetch angular from the browser scope */
