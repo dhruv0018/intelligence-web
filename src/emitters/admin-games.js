a75d7710bcd8ade6f33643302860fcac395ae0de
Michael <michaels3491@gmail.com>
2015-11-24 17:28:49 -0500

    feat(admin-games): adding admin games event emitter

---
+++ src/emitters/admin-games.js
@@ -0,0 +1,37 @@
+/* Import Angular IntelligenceWebClient module */
+const pkg = require('../../package.json');
+const angular = window.angular;
+const IntelligenceWebClient = angular.module(pkg.name);
+
+/* Import EventEmitter Node module */
+const EventEmitter = require('events').EventEmitter;
+
+/* Import EVENT types constant */
+import EVENT from '../constants/event';
+
+class AdminGamesEventEmitter extends EventEmitter {
+
+    /**
+     * Binds desired GlobalEventHandlers to class methods
+     * @constructor
+     * @returns {AdminGamesEventEmitter}
+     */
+    constructor () {
+        super();
+        this.setMaxListeners(10);
+    }
+
+    onQueryFinish (event) {
+        console.log('firing');
+        this.emit(EVENT.ADMIN.QUERY.COMPLETE, event);
+    }
+}
+
+/* Singleton */
+const adminGamesEventEmitter = new AdminGamesEventEmitter();
+
+/* Open singleton to Angular DI */
+IntelligenceWebClient.factory('AdminGamesEventEmitter', () => adminGamesEventEmitter);
+
+/* Export singleton for ES6, non-Angular modules */
+export default adminGamesEventEmitter;
