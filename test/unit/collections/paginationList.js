2df9b2a363f6da24ebf5852b389a03dd3922c287
Dave Krstin <david@krossover.com>
2015-11-27 14:13:03 -0500

    test(Pagination List): Add test file for Pagination List
    Add test file for Pagination List.

---
+++ test/unit/collections/paginationList.js
@@ -0,0 +1,9 @@
+import PaginationList from '../../../src/collections/paginationList';
+
+describe('PaginationList', () => {
+
+    it('should exist.', () => {
+
+        expect(PaginationList).to.exist;
+    });
+});
