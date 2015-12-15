8fd2358a5bc95fc1762671ac78d2b3fa117bf4f5
Dave Krstin <david@krossover.com>
2015-12-15 14:52:04 -0500

    test(Date Helper): Allow multiple properties
    Allow multiple properties for Date normalization.

--- test/unit/helpers/normalize-times.js
+++ test/unit/helpers/normalize-times.js
@@ -4,19 +4,33 @@ const moment = require('moment');
  * difference when creating shares in the Game Factory and creating
  * test data to test against. */
 
-function normalizeTimes (collection, dateProperty) {
+function setSecondsToZero (dateObject) {
+
+    return moment.utc(dateObject)
+        .milliseconds(0)
+        .seconds(0)
+        .toDate();
+}
+
+function normalizeTimes (collection, dateProperties) {
+
+    if (!Array.isArray(dateProperties)) {
+
+        dateProperties = [dateProperties];
+    }
 
     Object.keys(collection)
         .forEach(key => {
 
             let share = collection[key];
-            if (share[dateProperty]) {
 
-                share[dateProperty] = moment.utc(share[dateProperty])
-                    .milliseconds(0)
-                    .seconds(0)
-                    .toDate();
-            }
+            dateProperties.forEach(property => {
+
+                if (share[property]) {
+
+                    share[property] = setSecondsToZero(share[property]);
+                }
+            });
         });
 
     return collection;
