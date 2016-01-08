7b9c64f9163eedd4a88a303e5e948bc14535200d
Michael <michaels3491@gmail.com>
2016-01-08 15:04:03 -0500

    test(games resolution): initial tests for games resolution service

---
+++ test/unit/services/games-resolution.js
@@ -0,0 +1,30 @@
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+describe.only('GamesResolutionService', function() {
+
+    beforeEach(angular.mock.module('intelligence-web-client'));
+
+    it('should exist', inject(function(GamesResolutionService) {
+        console.log(GamesResolutionService);
+        expect(GamesResolutionService).to.exist;
+    }));
+
+    it('should have public API', inject(function(GamesResolutionService) {
+        expect(GamesResolutionService).to.have.property('queryFilter');
+        expect(GamesResolutionService).to.have.property('start');
+        expect(GamesResolutionService).to.have.property('isQuerying');
+        expect(GamesResolutionService).to.have.property('totalCount');
+        expect(GamesResolutionService).to.respondTo('query');
+    }));
+
+    it('should be able to set a queryfilter', inject(function(GamesResolutionService) {
+        let filter = {
+            status: 1
+        };
+        GamesResolutionService.queryFilter = filter;
+        expect(GamesResolutionService.queryFilter).to.equal(filter);
+    }));
+
+});
