7ca99b5150e74865eeec7746c37e0b20c33f9ada
Rama Sagar <sagar@krossover.com>
2017-03-08 19:21:29 +0530

    INTWEB-989, enable arena chart for ice hockey
    INTWEB-989, fix up ice-hockey filters styling

--- src/services/game-states.js
+++ src/services/game-states.js
@@ -80,7 +80,7 @@ function GameStatesService (
                                     states.push({name: 'Games.ArenaChart'});
                                 }
                                 break;
-                            case SPORTS.LACROSSE.id: case SPORTS.SOCCER.id:
+                            case SPORTS.LACROSSE.id: case SPORTS.SOCCER.id: case SPORTS.ICE_HOCKEY.id:
                                 if (features.isEnabled('ArenaChart')) {
                                     states.push({name: 'Games.ArenaChart'});
                                 }
